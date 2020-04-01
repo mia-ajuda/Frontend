2f0c07c14c922a311bb8aba1ff18d3dfc5e06df9
EsioFreitas <esio.gustavo@gmail.com>
2020-04-01 18:05:39 -0300

    Add comtainer component

--- app/src/pages/Main/index.js
+++ app/src/pages/Main/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import styles from "./styles";
+import Container from "../../components/Container";
 
 export default function Main() {
   return (
-    <View style={styles.container}>
+    <Container style={styles.container}>
       <Text style={styles.text1}>Projeto Mia Ajuda em Ação em h1</Text>
       <Text style={styles.text2}>Projeto Mia Ajuda em Ação em h2</Text>
       <Text style={styles.text3}>Projeto Mia Ajuda em Ação em p</Text>
-    </View>
+    </Container>
   );
 }
