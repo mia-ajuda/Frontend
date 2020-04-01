2185a61114cea9bd1faf8aa6619e2a93ad22dc71
Lucas Souza <lucas.soaresouza@gmail.com>
2020-04-01 20:00:02 -0300

    Adding marker on map
    Co-authored-by: gabrieldvpereira <gdavi.gd@gmail.com>

--- app/src/pages/Main/index.js
+++ app/src/pages/Main/index.js
@@ -1,19 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import styles from "./styles";
-import { MapView, Marker } from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import {
   requestPermissionsAsync,
   getCurrentPositionAsync
 } from "expo-location";
 
 export default function Main() {
-  const [currentRegion, setCurrentRegion] = useState({
-    latitude: 0,
-    longitude: 0,
-    latitudeDelta: 0.4,
-    longitudeDelta: 0.4
-  });
+  const [currentRegion, setCurrentRegion] = useState(null)
+  console.log(currentRegion)
   useEffect(() => {
     async function getLocation() {
       const { granted } = await requestPermissionsAsync();
@@ -21,13 +17,12 @@ export default function Main() {
         const { coords } = await getCurrentPositionAsync({
           enableHighAccuracy: true
         });
-        console.log("coords");
         const { latitude, longitude } = coords;
         setCurrentRegion({
           latitude,
           longitude,
-          latitudeDelta: 0.4,
-          longitudeDelta: 0.4
+          latitudeDelta: 0.025,
+          longitudeDelta: 0.025
         });
       }
     }
@@ -35,7 +30,23 @@ export default function Main() {
   }, []);
   return (
     <View style={styles.container}>
-      <MapView initialRegion={currentRegion} style={styles.map}></MapView>
+      <MapView 
+        initialRegion={currentRegion} 
+        style={styles.map}>
+         { currentRegion && (<Marker
+            coordinate={
+              {
+                latitude: currentRegion.latitude,
+                longitude: currentRegion.longitude  
+              }
+            }
+            draggable
+            onDragEnd = {(newCoordinates) => {
+              const {latitude, longitude} = newCoordinates.nativeEvent.coordinate
+              setCurrentRegion({...currentRegion, latitude, longitude})
+            }} 
+          />)}
+      </MapView>
     </View>
   );
 }
