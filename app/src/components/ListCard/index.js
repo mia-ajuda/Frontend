f068ec2972e7464a5fcb6a356d9cbb39b95ec15f
Hugo Sobral <hugosobral10@gmail.com>
2020-04-01 18:30:12 -0300

    #31 - Improve ListCard stylesheet Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>

--- app/src/components/ListCard/index.js
+++ app/src/components/ListCard/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import { Card } from "react-native-elements";
 
 import styles from "./styles";
 
 export default class ListCard extends Component {
   render() {
     return (
-      <Card style={styles.cardContainer}>
+      <View style={styles.cardContainer}>
         <View style={styles.cardTitle}>
           <Text style={styles.titleContent}>Mia Ajuda pufavô</Text>
         </View>
@@ -15,10 +14,13 @@ export default class ListCard extends Component {
           <Text style={styles.descriptionContent}>
             Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
             Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
-            Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
           </Text>
+
+          <View style={styles.categoryWarning}>
+            <Text style={styles.categoryName}>Ajudão</Text>
+          </View>
         </View>
-      </Card>
+      </View>
     );
   }
 }
