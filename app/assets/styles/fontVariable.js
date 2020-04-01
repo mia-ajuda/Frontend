83fdefc477596eb1117749676caa9fa865532ace
EsioFreitas <esio.gustavo@gmail.com>
2020-04-01 18:26:03 -0300

    Refactory color text dont variables

--- app/assets/styles/fontVariable.js
+++ app/assets/styles/fontVariable.js
@@ -1,16 +1,21 @@
+import colors from "./colorVariables";
+
 const minimumTextSize = 16;
 const fontFamily = "montserrat-regular";
 
 const fonts = {
   title: {
+    color: colors.dark,
     fontFamily: fontFamily,
     fontSize: minimumTextSize * 1.5
   },
   subtitle: {
+    color: colors.dark,
     fontFamily: fontFamily,
     fontSize: minimumTextSize * 1.3
   },
   body: {
+    color: colors.dark,
     fontFamily: fontFamily,
     fontSize: minimumTextSize
   }
--- app/src/pages/Main/index.js
+++ app/src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import styles from "./styles";
 import Container from "../../components/Container";
+import { Button } from "react-native";
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <Text style={styles.text1}>Projeto Mia Ajuda em Ação em h1</Text>
       <Text style={styles.text2}>Projeto Mia Ajuda em Ação em h2</Text>
       <Text style={styles.text3}>Projeto Mia Ajuda em Ação em p</Text>
+      <Button title="Um titulo graaaaaaaaaaaaaaaaande"/>
     </Container>
   );
 }
