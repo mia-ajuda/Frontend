47a81d2d4f94401c506106e03db5aa70068b704e
Hugo Sobral <hugosobral10@gmail.com>
2020-04-02 16:54:22 -0300

    #31 - Finish ListCard style
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>

--- app/src/components/ListCard/index.js
+++ app/src/components/ListCard/index.js
@@ -4,27 +4,23 @@ import { View, Text } from "react-native";
 import styles from "./styles";
 
 export default class ListCard extends Component {
-  
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
     return (
       <View style={styles.cardContainer}>
         <View style={styles.cardTitle}>
-          <Text style={styles.titleContent}>
-            { this.props.helpInfo.title }
-          </Text>
+          <Text style={styles.titleContent}>Titulo Genérico</Text>
         </View>
         <View style={styles.cardDescription}>
           <Text style={styles.descriptionContent}>
-            { this.props.helpInfo.description } 
+            Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
+            Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
           </Text>
           <View style={styles.categoryWarning}>
-            <Text style={styles.categoryName}>
-              { this.props.helpInfo.category.title }
-            </Text>
+            <Text style={styles.categoryName}>Compras</Text>
           </View>
         </View>
       </View>
