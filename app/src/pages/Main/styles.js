aeb425bbb18b884b03277397f54ce2136bd33db1
Gabriel Davi <gdavi.gd@gmail.com>
2020-04-02 16:28:59 -0300

    #US12 - rebase with devel

--- app/src/pages/Main/styles.js
+++ app/src/pages/Main/styles.js
@@ -1,6 +1,4 @@
 import { StyleSheet } from "react-native";
-import fonts from "../../../assets/styles/fontVariable";
-import colors from "../../../assets/styles/colorVariables";
 
 const styles = StyleSheet.create({
   container: {
