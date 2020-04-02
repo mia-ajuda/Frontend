bb2926056fa3bba5b64d19c9da0b5b2d211e0e6d
LeoSilvaGomes <leonardodasigomes@gmail.com>
2020-04-02 18:25:25 -0300

    #31 - Adjust list logic
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- app/src/components/HelpList/styles.js
+++ app/src/components/HelpList/styles.js
@@ -1,5 +1,22 @@
 import { StyleSheet } from "react-native";
 
+import colors from "../../../assets/styles/colorVariables";
+
 export default StyleSheet.create({
 
+    helpListContainer: {
+        backgroundColor: colors.primary,
+        width: "100%", 
+        height: "70%",
+        
+        alignSelf: "flex-end",
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+    },
+
+    listContent: {
+        width: "90%",
+
+        alignSelf: "center"
+    }
 });
