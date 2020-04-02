2f68d8a2254b2e1b43eac7f5e1a83861fad64f9d
LeoSilvaGomes <leonardodasigomes@gmail.com>
2020-04-02 15:59:55 -0300

    #31 Create structure HelpList component and adjust style ListCard
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- app/src/components/ListCard/index.js
+++ app/src/components/ListCard/index.js
@@ -4,20 +4,27 @@ import { View, Text } from "react-native";
 import styles from "./styles";
 
 export default class ListCard extends Component {
+  
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <View style={styles.cardContainer}>
         <View style={styles.cardTitle}>
-          <Text style={styles.titleContent}>Mia Ajuda pufavô</Text>
+          <Text style={styles.titleContent}>
+            { this.props.helpInfo.title }
+          </Text>
         </View>
         <View style={styles.cardDescription}>
           <Text style={styles.descriptionContent}>
-            Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
-            Descrição da ajuda. Descrição da ajuda. Descrição da ajuda.
+            { this.props.helpInfo.description } 
           </Text>
-
           <View style={styles.categoryWarning}>
-            <Text style={styles.categoryName}>Ajudão</Text>
+            <Text style={styles.categoryName}>
+              { this.props.helpInfo.category.title }
+            </Text>
           </View>
         </View>
       </View>
