15fee87c052b4e0d1d5ac646a6427836031da1cc
Luis Taira <luishptaira@gmail.com>
2020-04-04 14:13:06 -0300

    Making button only active when there is text
    Co-authored-by: EsioFreita <esio.gustavo@gmail.com>
    Co-authored-by: gabrieldvpereira <gdavi.gd@gmail.com>

--- app/src/pages/helpPages/createHelp/index.js
+++ app/src/pages/helpPages/createHelp/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Picker, Text } from "react-native";
 import styles from "./styles";
 import Container from "../../../components/Container";
 import Input from "../../../components/UI/input";
 import Button from "../../../components/UI/button";
-import { ScrollView } from "react-native-gesture-handler";
 
-export default function CreateHelp({ navigation }) {
+export default function CreateHelp() {
+
+  let [title, setTitle] = useState("");
+  let [category, setCategory] = useState("");
+  let [description, setDescription] = useState("");
+  let [buttonDisabled, setButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    if (title && category && description) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [title, category, description]);
+
   return (
     <View style={{
       height: '100%',
@@ -15,21 +28,23 @@ export default function CreateHelp({ navigation }) {
       justifyContent: 'space-around',
     }}>
       <Container style={styles.container}>
-        <Input label="Título" />
+        <Input label="Título" change={(text) => setTitle(text.nativeEvent.text)} />
         <View>
           <Text style={styles.label}>Categoria</Text>
           <View style={styles.picker}>
-            <Picker label="Categoria">
+            <Picker label="Categoria" selectedValue={category} onValueChange={(itemValue) => setCategory(itemValue)}>
               <Picker.Item label="" value="" />
-              <Picker.Item label="Java" value="java" />
-              <Picker.Item label="JavaScript" value="js" />
+              <Picker.Item label="Categoria 1" value="Categoria 1" />
+              <Picker.Item label="Categoria 2" value="Categoria 2" />
+              <Picker.Item label="Categoria 3" value="Categoria 3" />
+              <Picker.Item label="Categoria 4" value="Categoria 4" />
             </Picker>
           </View>
         </View>
-        <Input label="Descrição" textarea />
+        <Input label="Descrição" textarea change={(text) => setDescription(text.nativeEvent.text)} />
       </Container>
       <Container style={styles.btnContainer}>
-          <Button title="Preciso de ajuda" large />
+        <Button title="Preciso de ajuda" large disabled={buttonDisabled} />
       </Container>
     </View>
   );
