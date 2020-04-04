02b951c997b970e15399beb32f54bcca1384a0fa
EsioFreitas <esio.gustavo@gmail.com>
2020-04-04 18:51:16 -0300

    Refactory de view help page position

--- app/src/components/Container/styles.js
+++ app/src/components/Container/styles.js
@@ -2,7 +2,8 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    margin: 16
+    margin: 16,
+    height: "100%"
   }
 });
 
--- app/src/pages/helpPages/createHelp/styles.js
+++ app/src/pages/helpPages/createHelp/styles.js
@@ -3,50 +3,36 @@ import fonts from "../../../../assets/styles/fontVariable";
 import colors from "../../../../assets/styles/colorVariables";
 
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
     height: "100%",
     flexDirection: "column",
     justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#f7f7f7"
   },
-  text1: {
-    ...fonts.title,
-    fontFamily: "montserrat-semibold"
-  },
-  text2: {
-    ...fonts.subtitle
-  },
-  text3: {
-    ...fonts.body
-  },
-  btn: {
-    backgroundColor: colors.danger,
-    color: colors.danger
+  margiView: {
+    marginVertical: 12,
   },
   btnContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    height: "20%",
   },
   input: {
     ...fonts.body,
     borderWidth: 2,
     borderRadius: 5,
     borderColor: colors.primary,
-    padding: 10
+    padding: 10,
   },
   picker: {
     ...fonts.body,
     borderWidth: 2,
     borderRadius: 5,
-    borderColor: colors.primary
+    borderColor: colors.primary,
   },
   label: {
     ...fonts.body,
     fontFamily: "montserrat-semibold",
-    color: colors.primary
-  }
+    color: colors.primary,
+  },
 });
 
 export default styles;
