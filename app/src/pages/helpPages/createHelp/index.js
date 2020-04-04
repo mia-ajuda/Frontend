d53836f1acfcba9a490d6d1a8235dd4b8d973fbe
EsioFreitas <esio.gustavo@gmail.com>
2020-04-04 18:51:16 -0300

    Refactory de view help page position

--- app/src/pages/helpPages/createHelp/index.js
+++ app/src/pages/helpPages/createHelp/index.js
@@ -6,7 +6,6 @@ import Input from "../../../components/UI/input";
 import Button from "../../../components/UI/button";
 
 export default function CreateHelp() {
-
   let [title, setTitle] = useState("");
   let [category, setCategory] = useState("");
   let [description, setDescription] = useState("");
@@ -21,31 +20,42 @@ export default function CreateHelp() {
   }, [title, category, description]);
 
   return (
-    <View style={{
-      height: '100%',
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-    }}>
-      <Container style={styles.container}>
-        <Input label="Título" change={(text) => setTitle(text.nativeEvent.text)} />
+    <Container>
+      <View style={styles.view}>
         <View>
-          <Text style={styles.label}>Categoria</Text>
-          <View style={styles.picker}>
-            <Picker label="Categoria" selectedValue={category} onValueChange={(itemValue) => setCategory(itemValue)}>
-              <Picker.Item label="" value="" />
-              <Picker.Item label="Categoria 1" value="Categoria 1" />
-              <Picker.Item label="Categoria 2" value="Categoria 2" />
-              <Picker.Item label="Categoria 3" value="Categoria 3" />
-              <Picker.Item label="Categoria 4" value="Categoria 4" />
-            </Picker>
+          <Input
+            label="Título"
+            change={(text) => setTitle(text.nativeEvent.text)}
+          />
+          <View style={styles.margiView} />
+          <View>
+            <Text style={styles.label}>Categoria</Text>
+            <View style={styles.picker}>
+              <Picker
+                label="Categoria"
+                selectedValue={category}
+                onValueChange={(itemValue) => setCategory(itemValue)}
+              >
+                <Picker.Item label="" value="" />
+                <Picker.Item label="Categoria 1" value="Categoria 1" />
+                <Picker.Item label="Categoria 2" value="Categoria 2" />
+                <Picker.Item label="Categoria 3" value="Categoria 3" />
+                <Picker.Item label="Categoria 4" value="Categoria 4" />
+              </Picker>
+            </View>
           </View>
+          <View style={styles.margiView} />
+          <Input
+            label="Descrição"
+            textarea
+            change={(text) => setDescription(text.nativeEvent.text)}
+          />
         </View>
-        <Input label="Descrição" textarea change={(text) => setDescription(text.nativeEvent.text)} />
-      </Container>
-      <Container style={styles.btnContainer}>
-        <Button title="Preciso de ajuda" large disabled={buttonDisabled} />
-      </Container>
-    </View>
+
+        <View style={styles.btnContainer}>
+          <Button title="Preciso de ajuda" large disabled={buttonDisabled} />
+        </View>
+      </View>
+    </Container>
   );
 }
