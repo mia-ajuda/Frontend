901fe2477f39e3ee46e7d49b91164074e31c3bd5
Hugo Sobral <hugosobral10@gmail.com>
2020-04-07 16:56:14 -0300

    #31 - Config help services
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>

--- app/src/services/Help.js
+++ app/src/services/Help.js
@@ -1,20 +1,30 @@
-import api from "../services/Api";
+import api from "./Api";
 
-export default class HelpService {
+class HelpService {
   constructor() {}
 
-  getAllHelp() {}
+  getAllHelps = async (userId = null, status = null) => {
+    let url = "/help";
+
+    if (userId && status) {
+      url += `?id.except=${userId}&status=${status}`;
+    }
+
+    if (userId && status === null) {
+      url += `?id=${userId}`;
+    }
+
+    const allHelps = await api.get(url);
+    return allHelps.data;
+  };
 
   getAllHelpForCategory() {}
   getAllHelpForUser() {}
   getAllHelpForHelper() {}
-
   createHelp() {}
-
   deleteHelp() {}
 }
 
 const helpService = new HelpService();
 Object.freeze(helpService);
-
 export default helpService;
