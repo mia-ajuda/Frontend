4e7b4c865f44a2f56127ef5046d81145066df382
Lucas Souza <lucas.soaresouza@gmail.com>
2020-04-07 22:51:57 -0300

    Adding title and instructions in get location page

--- app/src/pages/authPages/Location/styles.js
+++ app/src/pages/authPages/Location/styles.js
@@ -5,39 +5,68 @@ import fonts from "../../../../assets/styles/fontVariable";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    alignContent: 'center',
+    justifyContent: 'space-around',
+  },
+  textMapContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignContent: 'flex-end',
+    justifyContent: 'space-around',
   },
   titleBox: {
+    position: 'absolute',
     backgroundColor: '#fff',
     borderRadius: 20,
-    opacity: 0.75,
-    marginTop: -170,
-    marginHorizontal: 40,
-    padding: 25,
+    padding: 10,
     shadowColor: '#000',
-    elevation: 5,
+    elevation: 8,
+    alignContent: 'center',
+    alignSelf: 'center',
+    width: "90%",
+    marginTop: "20%",
   },
   title: {
     ...fonts.body,
-    color: colors.primary
+    color: colors.black,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
-  button: {
-    ...fonts.title,
-    color: colors.dark
+  instructionBox: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 10,
+    shadowColor: '#000',
+    elevation: 8,
+    alignSelf: 'center',
+    alignContent: 'center',
+    flexDirection: 'column',
+    width: "90%",
+    height: "7%",
+    marginTop: "135%"
+  },
+  instruction: {
+    ...fonts.body,
+    color: colors.primary,
+    textAlign: 'justify',
+    fontSize: 18, 
   },
   map: {
     width: "100%",
     height: "100%"
   },
   locationButton: {
-    marginTop: -200,
     width: "40%",
     height: 50,
+    marginTop: -100,
   },
   buttonsBox: {
     flexDirection: 'row',
     alignContent: 'flex-start',
     justifyContent: 'space-around',
-    marginTop: 120
   },
 });
 
