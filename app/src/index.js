4237b82c79b9e4cdf459a9b32fdcd1bf30994e55
phe0 <pheofo@gmail.com>
2020-04-10 11:44:06 -0300

    test notification

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import Main from "./pages/Main";
 import colors from "../assets/styles/colorVariables";
 import { ThemeProvider } from "react-native-elements";
 import Routes from "./routes";
+import { Platform } from 'react-native';
 import { UserContextProvider } from "./store/contexts/userContext";
+
 
 export default function Root() {
   const theme = {
@@ -13,6 +15,7 @@ export default function Root() {
       warning: colors.secondary,
     },
   };
+
   return (
     <UserContextProvider>
       <Routes />
--- app/src/pages/authPages/SignUp/index.js
+++ app/src/pages/authPages/SignUp/index.js
@@ -3,9 +3,39 @@ import React, { useContext } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import { UserContext } from "../../../store/contexts/userContext";
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 export default function SignUp({ navigation }) {
   const { user } = useContext(UserContext);
+
+  async function getPushNotificationPermissions() {
+
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      return;
+    }
+
+    console.log('blah')
+    const token = await Notifications.getExpoPushTokenAsync()
+
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('chat-messages', {
+        name: 'Chat messages',
+        sound: true,
+      });
+    }
+
+    console.log(token)
+    return token;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>SignUp page </Text>
@@ -24,6 +54,10 @@ export default function SignUp({ navigation }) {
       <TouchableOpacity onPress={() => navigation.goBack()}>
         <Text style={styles.button}>Back</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={() => getToken()}>
+        <Text style={styles.button}>Token</Text>
+      </TouchableOpacity>
+      <Text>{getPushNotificationPermissions()}</Text>
     </View>
   );
 }
