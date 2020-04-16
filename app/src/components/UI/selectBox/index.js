a7710888a38d95cb5279625ff6e0995784914b13
Gabriel Davi <gdavi.gd@gmail.com>
2020-04-16 16:03:12 -0300

    Fix select box style and button

--- app/src/components/UI/selectBox/index.js
+++ app/src/components/UI/selectBox/index.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { CheckBox } from "react-native-elements";
 import styles from "./styles";
-import colors from "../../../../assets/styles/colorVariables"
-
 export default function SelectBox({
   setSelectedCategoryArray,
   filterCategoryArray,
   category,
   selectedCategoryArray,
-  title,
-  onChange,
-  select=false
 }) {
   const [selected, setSelect] = useState(
     filterCategoryArray.some((categoryId) => categoryId === category._id)
   );
-  const showTitle = category.name || title;
 
   useEffect(() => {
     if (selected) {
@@ -31,14 +25,11 @@ export default function SelectBox({
   return (
     <>
       <CheckBox
-        title={showTitle}
+        title={category.name}
         textStyle={styles.checkBoxText}
         checkedIcon="check-square"
-        checked={select}
-        onPress={onChange}
-        containerStyle={styles.container}
-        checkedColor={colors.primary}
-        uncheckedColor={colors.primary}
+        checked={selected}
+        onPress={() => setSelect(!selected)}
       />
     </>
   );
--- app/src/components/UI/selectBox/styles.js
+++ app/src/components/UI/selectBox/styles.js
@@ -1,18 +1,9 @@
 import { StyleSheet } from "react-native";
-import colors from "../../../../assets/styles/colorVariables"
-
 
 const styles = StyleSheet.create({
   checkBoxText: {
     fontSize: 15,
   },
-  container: {
-    backgroundColor: colors.light,
-    justifyContent: 'flex-start',
-    padding: 0,
-    borderColor: colors.light,
-    marginLeft: 0
-  }
 });
 
 export default styles;
