7073646da8d22ab52d24597fb53a3f64d56bfa24
Joberth Rogers <joberth.rogers18@gmail.com>
2020-04-20 15:56:40 -0400

    Added spinner to load while verify if user exists
    Co-authored-by: Matheus Estanislau <matheus.estanislau7@gmail.com>

--- app/src/pages/authPages/Login/index.js
+++ app/src/pages/authPages/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   KeyboardAvoidingView,
@@ -13,8 +13,7 @@ import {
 import UserService from "../../../services/User";
 import Button from "../../../components/UI/button";
 import { Icon } from "react-native-elements";
-import { UserContext } from "../../../store/contexts/userContext";
-import actions from "../../../store/actions";
+import colors from '../../../../assets/styles/colorVariables';
 
 import styles from "./styles";
 import { UserContext } from "../../../store/contexts/userContext";
@@ -22,12 +21,12 @@ import actions from "../../../store/actions";
 
 export default function Login({ navigation }) {
   const { dispatch } = useContext(UserContext);
-
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [buttonDisabled, setButtonDisabled] = useState(false);
   const [isLoading, setLoading] = useState(false);
-  const [keyboardShow, setKeyboardShow] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (email && password) {
@@ -51,11 +50,9 @@ export default function Login({ navigation }) {
     setLoading(true);
 
     try {
-      const user = await UserService.logIn(data);
-      if (user) {
-        setLoading(false);
-        dispatch({ type: actions.user.storeUserInfo, data: user });
-      }
+      await UserService.logIn(data);
+
+      navigation.navigate("main");
     } catch (err) {
       Alert.alert(
         "Ooops..",
@@ -68,9 +65,10 @@ export default function Login({ navigation }) {
       setLoading(false);
     }
   };
-  
+
   const loginHandlerFacebook = async () => {
     try {
+      setLoading(true);
       await UserService.logInWithFacebook(navigation);
     } catch (err) {
       Alert.alert(
@@ -85,18 +83,23 @@ export default function Login({ navigation }) {
         {
           cancelable: false
         }
-        );
-      }
-    };
-    
-    const loginHandlerGoogle = async () => {
-      try {
-        await UserService.loginInWithGoogle(navigation);
-      } catch (err) {
-        Alert.alert("Erro", err.error, [{ text: "OK", onPress: () => {} }], {
-          cancelable: false
-        });
+      );
     }
+
+    setLoading(false);
+  };
+
+  const loginHandlerGoogle = async () => {
+    try {
+      setLoading(true);
+      await UserService.loginInWithGoogle(navigation);
+    } catch (err) {
+      Alert.alert("Erro", err.error, [{ text: "OK", onPress: () => {} }], {
+        cancelable: false
+      });
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -105,92 +108,93 @@ export default function Login({ navigation }) {
       behavior={Platform.OS === "ios" ? "padding" : null}
       keyboardVerticalOffset={Platform.OS === "ios" ? 5 : 0}
     >
-      <View style={styles.logo}>
-        <Image
-          style={{ flex: 1, resizeMode: "contain", marginTop: 30 }}
-          source={require("../../../images/logo.png")}
-        />
-      </View>
-      <View style={styles.input}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Email"
-          autoCorrect={false}
-          placeholderTextColor="#FFF"
-          onChangeText={emailHandler}
-          value={email}
-        />
-
-        <TextInput
-          style={styles.textInput}
-          secureTextEntry
-          placeholderTextColor="#FFF"
-          placeholder="Senha"
-          autoCorrect={false}
-          onChangeText={passwordHandler}
-          value={password}
-        />
-        <View style={styles.forgotPassword}>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("forgotPassword");
-            }}
-          >
-            <Text style={styles.forgotPasswordtext}>Esqueceu a senha?</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={{ alignItems: "flex-end", width: "90%" }}>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("forgotPassword");
-          }}
-        >
-          <Text style={styles.forgotPasswordtext}>Esqueceu a senha?</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.viewLogin}>
-        {isLoading ? (
-          <ActivityIndicator size="large" color="#fff" />
+      {
+        !loading ? 
+        (
+          <>
+            <View style={styles.logo}>
+              <Image
+                style={{ flex: 1, resizeMode: "contain", marginTop: 30 }}
+                source={require("../../../images/logo.png")}
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Email"
+                autoCorrect={false}
+                placeholderTextColor="#FFF"
+                onChangeText={emailHandler}
+                value={email}
+              />
+      
+              <TextInput
+                style={styles.textInput}
+                secureTextEntry
+                placeholderTextColor="#FFF"
+                placeholder="Senha"
+                autoCorrect={false}
+                onChangeText={passwordHandler}
+                value={password}
+              />
+              <View style={styles.forgotPassword}>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("forgotPassword");
+                  }}
+                >
+                  <Text style={styles.forgotPasswordtext}>Esqueceu a senha?</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+      
+            <View style={styles.viewBtn}>
+              <View style={styles.login}>
+                <Button
+                  large
+                  type="white"
+                  title="ENTRAR"
+                  press={loginHandler}
+                  disabled={buttonDisabled}
+                />
+              </View>
+              <TouchableOpacity
+                style={styles.signUP}
+                onPress={async () => {
+                  navigation.navigate("registrationData");
+                }}
+              >
+                <Text style={styles.signupText}>Não tem uma conta?</Text>
+              </TouchableOpacity>
+              <View style={styles.quickLogin}>
+                <View style={styles.viewGoogle}>
+                  <TouchableOpacity
+                    style={styles.btnGoogle}
+                    onPress={loginHandlerGoogle}
+                  >
+                    <Icon type="antdesign" name={"google"} color={"white"} />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.viewFacebook}>
+                  <TouchableOpacity
+                    style={styles.btnFacebook}
+                    onPress={loginHandlerFacebook}
+                  >
+                    <Icon type="font-awesome" name={"facebook"} color={"white"} />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </>
         ) : (
-          <Button
-            style={styles.login}
-            large
-            type="white"
-            title="ENTRAR"
-            press={loginHandler}
-            disabled={buttonDisabled}
-          />
-        )}
-
-        <TouchableOpacity
-          style={styles.signUP}
-          onPress={ async() => {
-            await AsyncStorage.setItem('hasUser', false);
-            navigation.navigate("registrationData");
-          }}
-        >
-          <Text style={styles.signupText}>Não tem uma conta?</Text>
-        </TouchableOpacity>
-        <View style={styles.quickLogin}>
-          <View style={styles.viewGoogle}>
-            <TouchableOpacity
-              style={styles.btnGoogle}
-              onPress={loginHandlerGoogle}
-            >
-              <Icon type="antdesign" name={"google"} color={"white"} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.viewFacebook}>
-            <TouchableOpacity
-              style={styles.btnFacebook}
-              onPress={loginHandlerFacebook}
-            >
-              <Icon type="font-awesome" name={"facebook"} color={"white"} />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
+          <>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <ActivityIndicator size="large" color={colors.light} />
+            </View>
+          </>
+        )
+      }
+     
     </KeyboardAvoidingView>
   );
 }
