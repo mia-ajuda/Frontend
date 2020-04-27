9aa3a88ab17744d4b6fcbb4b1194ca44e02fd0dd
Hugo Sobral <hugosobral10@gmail.com>
2020-04-27 19:21:33 -0300

    Add plural form treatment for notification time

--- app/src/components/NotificationCard/index.js
+++ app/src/components/NotificationCard/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Icon } from "react-native-elements";
 
@@ -6,69 +6,73 @@ import colors from "../../../assets/styles/colorVariables";
 import styles from "./styles";
 
 export default function NotificationCard({
-  notificationType, 
-  notificationTitle, 
-  notificationBody, 
-  notificationDate
+  notificationType,
+  notificationTitle,
+  notificationBody,
+  notificationDate,
 }) {
-
   const [time, setTime] = useState(``);
   const [iconName, setIconName] = useState("bell");
   const [iconBackground, setIconBackground] = useState(colors.primary);
-  
+
   useEffect(() => {
-    let date = new Date(notificationDate)
-    let dateNow = new Date()
-    let interval = dateNow.getTime() - date.getTime()
+    let date = new Date(notificationDate);
+    let dateNow = new Date();
+    let interval = dateNow.getTime() - date.getTime();
 
-    if(interval > 31536000000){
-      let year = dateNow.getFullYear() - date.getFullYear()
-      setTime(`${year} anos atrás`)
+    if (interval > 31536000000) {
+      let year = dateNow.getFullYear() - date.getFullYear();
+      year > 1 ? setTime(`${year} anos atrás`) : setTime(`${year} ano atrás`);
     } else if (interval > 2678400000) {
-      let month = dateNow.getMonth() - date.getMonth()
-      setTime(`${month} meses atrás`)
+      let month = dateNow.getMonth() - date.getMonth();
+      month > 1
+        ? setTime(`${month} mêses atrás`)
+        : setTime(`${month} mês atrás`);
     } else if (interval > 86400000) {
-      let date = dateNow.getDate() - date.getDate()
-      setTime(`${date} dias atrás`)
-    } else if(interval > 3600000) {
-      let hours = dateNow.getHours() - date.getHours()
-      setTime(`${hours} horas atrás`)
-    } else if(interval > 60000) {
-      let minutes = dateNow.getMinutes() - date.getMinutes()
-      setTime(`${minutes} minutos atrás`)
+      let day = dateNow.getDate() - date.getDate();
+      day > 1 ? setTime(`${day} dias atrás`) : setTime(`${day} dia atrás`);
+    } else if (interval > 3600000) {
+      let hours = dateNow.getHours() - date.getHours();
+      hours > 1
+        ? setTime(`${hours} horas atrás`)
+        : setTime(`${hours} hora atrás`);
+    } else if (interval > 60000) {
+      let minutes = dateNow.getMinutes() - date.getMinutes();
+      minutes > 1
+        ? setTime(`${minutes} minutos atrás`)
+        : setTime(`${minutes} minuto atrás`);
     } else {
-      setTime(`agora`)
+      setTime("Agora");
     }
-
   }, [time]);
 
   useEffect(() => {
     switch (notificationType) {
       case "ajudaRecebida":
         setIconName("bell");
-        setIconBackground(colors.primary)
+        setIconBackground(colors.primary);
         break;
 
       case "ajudaAceita":
         setIconName("bell");
-        setIconBackground(colors.primary)
+        setIconBackground(colors.primary);
         break;
 
       case "ajudaFinalizada":
         setIconName("check");
-        setIconBackground(colors.danger)
+        setIconBackground(colors.danger);
         break;
 
       case "ajudaExpirada":
         setIconName("exclamation");
-        setIconBackground(colors.danger)
+        setIconBackground(colors.danger);
         break;
     }
   }, [iconName]);
 
   return (
     <View style={styles.cardContainer}>
-      <View style={[styles.iconContent, {backgroundColor: iconBackground}]}>
+      <View style={[styles.iconContent, { backgroundColor: iconBackground }]}>
         <Icon
           size={15}
           name={iconName}
@@ -81,9 +85,7 @@ export default function NotificationCard({
         <Text style={styles.title} numberOfLines={2}>
           {notificationTitle}
         </Text>
-        <Text numberOfLines={2}>
-          {notificationBody}
-        </Text>
+        <Text numberOfLines={2}>{notificationBody}</Text>
         <Text style={styles.time}>{time}</Text>
       </View>
     </View>
