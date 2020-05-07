0988b0eab615b1c7735e154d82c6e181e0b5bb39
Gabriel Davi <gdavi.gd@gmail.com>
2020-05-07 19:28:45 -0300

    Displaying profile image to the profile page

--- app/src/pages/profile/index.js
+++ app/src/pages/profile/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { View, AsyncStorage } from "react-native";
+import { View, AsyncStorage, Image } from "react-native";
 import styles from "./styles";
 import Button from "../../components/UI/button";
 import { UserContext } from "../../store/contexts/userContext";
@@ -7,18 +7,44 @@ import actions from "../../store/actions";
 
 export default function Profile() {
   const { user, dispatch } = useContext(UserContext);
+  const profilePhoto = user.photo
+    ? { uri: user.photo }
+    : require("../../../assets/images/blueLogo.png");
+
+  console.log(user);
   async function logout() {
     await AsyncStorage.removeItem("user");
     dispatch({ type: actions.user.removeUserInfo });
   }
   return (
     <View style={styles.container}>
-      <Button
-        press={() => {
-          logout();
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
         }}
-        title="sair"
-      />
+      >
+        <Image
+          source={profilePhoto}
+          style={{ width: 175, height: 175, borderRadius: 100 }}
+        />
+      </View>
+      <View
+        style={{
+          justifyContent: "flex-end",
+          alignItems: "center",
+          flex: 1,
+          marginBottom: 10,
+        }}
+      >
+        <Button
+          press={() => {
+            logout();
+          }}
+          title="sair"
+        />
+      </View>
     </View>
   );
 }
