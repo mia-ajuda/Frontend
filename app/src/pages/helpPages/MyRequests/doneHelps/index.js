91ca45a6618c7e76c5c769a5afc85be6e91b8386
Gabriel Davi <gdavi.gd@gmail.com>
2020-05-15 18:18:16 -0300

    Add loading indicator to my helps

--- app/src/pages/helpPages/MyRequests/doneHelps/index.js
+++ app/src/pages/helpPages/MyRequests/doneHelps/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext, useCallback } from "react";
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, ActivityIndicator } from "react-native";
 import ListCard from "../../../../components/ListCard";
 import { UserContext } from "../../../../store/contexts/userContext";
 import helpService from "../../../../services/Help";
 import styles from "../styles";
+import colors from "../../../../../assets/styles/colorVariables";
+
 import ConfirmationModal from "../../../../components/modals/confirmationModal";
 import NoHelps from "../../../../components/NoHelps";
 import { useFocusEffect } from "@react-navigation/native";
@@ -13,6 +15,7 @@ export default function DoneHelps({ navigation }) {
   const [confirmationModalVisible, setConfirmationModalVisible] = useState(
     false
   );
+  const [loadingHelps, setLoadingHelps] = useState(false);
 
   const { user } = useContext(UserContext);
   const { _id: userId } = user;
@@ -24,15 +27,17 @@ export default function DoneHelps({ navigation }) {
   );
 
   async function loadFinishedHelps() {
+    setLoadingHelps(true);
     let tempFinished = await helpService.getAllHelpForUser(userId, "finished");
-    let resFinished = tempFinished.filter(help => help.active === true);
+    let resFinished = tempFinished.filter((help) => help.active === true);
     setFinishedHelpList(resFinished);
+    setLoadingHelps(false);
   }
 
   async function excludeHelp(helpId) {
     try {
       await helpService.deleteHelp(helpId);
-      const updatedArray = onGoingHelpList.filter(help => {
+      const updatedArray = onGoingHelpList.filter((help) => {
         return help._id !== helpId;
       });
       setFinishedHelpList(updatedArray);
@@ -44,10 +49,14 @@ export default function DoneHelps({ navigation }) {
 
   return (
     <View>
-      {finishedHelpList.length > 0 ? (
+      {loadingHelps ? (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color={colors.primary} />
+        </View>
+      ) : finishedHelpList.length > 0 ? (
         <ScrollView>
           <View style={styles.helpList}>
-            {finishedHelpList.map(item => (
+            {finishedHelpList.map((item) => (
               <View key={item._id}>
                 <ListCard
                   helpTitle={item.title}
