c98a983aa0f922f8c1f0fa8acc7a018b9bd059f4
Pedro Féo <pheofo@gmail.com>
2020-05-22 20:05:34 -0300

    change babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:react-native-dotenv"],
+    presets: [
+      "module:metro-react-native-babel-preset",
+      "module:react-native-dotenv",
+    ],
   };
 };
