ee29a9e67e7dd15c0634480a7b102a4e8df1e154
Gabriel Davi <gdavi.gd@gmail.com>
2020-05-30 04:14:26 -0300

    Removed facebook and google auth for now

--- app/src/pages/authPages/Login/index.js
+++ app/src/pages/authPages/Login/index.js
@@ -15,7 +15,6 @@ import Button from "../../../components/UI/button";
 import { Icon } from "react-native-elements";
 import colors from "../../../../assets/styles/colorVariables";
 
-
 import styles from "./styles";
 import { UserContext } from "../../../store/contexts/userContext";
 import actions from "../../../store/actions";
@@ -27,10 +26,6 @@ export default function Login({ navigation }) {
   const [password, setPassword] = useState("");
   const [buttonDisabled, setButtonDisabled] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [loadingFace, setLoadingFace] = useState(false);
-  const [loadingGoogle, setLoadingGoogle] = useState(false);
-  const [facebookColor, setFacebookColor] = useState("#3B5998");
-  const [googleColor, setGoogleColor] = useState("#d93025");
 
   useEffect(() => {
     if (email && password) {
@@ -54,19 +49,13 @@ export default function Login({ navigation }) {
 
     try {
       setLoading(true);
-      setFacebookColor("#575757");
-      setFacebookColor("#616161");
+
       const user = await UserService.logIn(data);
       setLoading(false);
-      setGoogleColor("#d93025");
-      setFacebookColor("#3B5998");
-
       if (user) {
         dispatch({ type: actions.user.storeUserInfo, data: user });
       }
     } catch (err) {
-      setGoogleColor("#d93025");
-      setFacebookColor("#3B5998");
       setLoading(false);
       Alert.alert(
         "Ooops..",
@@ -79,68 +68,15 @@ export default function Login({ navigation }) {
     }
   };
 
-  const loginHandlerFacebook = async () => {
-    try {
-      setLoadingFace(true);
-      setGoogleColor("#616161");
-      const user = await UserService.logInWithFacebook(navigation);
-      setGoogleColor("#d93025");
-      setLoadingFace(false);
-
-      if (user) {
-        dispatch({ type: actions.user.storeUserInfo, data: user });
-      }
-    } catch (err) {
-      setGoogleColor("#d93025");
-      setLoadingFace(false);
-      Alert.alert(
-        "Erro",
-        err.error,
-        [
-          {
-            text: "OK",
-            onPress: () => {},
-          },
-        ],
-        {
-          cancelable: false,
-        }
-      );
-    }
-  };
-
-  const loginHandlerGoogle = async () => {
-    try {
-      setLoadingGoogle(true);
-      setFacebookColor("#575757");
-      const user = await UserService.loginInWithGoogle(navigation);
-      setLoadingGoogle(false);
-      setFacebookColor("#3B5998");
-
-      if (user) {
-        dispatch({ type: actions.user.storeUserInfo, data: user });
-      }
-    } catch (err) {
-      setFacebookColor("#3B5998");
-      setLoadingGoogle(false);
-      Alert.alert("Erro", err.error, [{ text: "OK", onPress: () => {} }], {
-        cancelable: false,
-      });
-    }
-  };
-  
   return (
     <View style={styles.container}>
       <View style={styles.logo}>
         <Image
-          style={{ flex: 1, resizeMode: "contain", marginTop: 30 }}
+          style={{ flex: 1, resizeMode: "contain" }}
           source={require("../../../images/logo.png")}
         />
       </View>
-      <KeyboardAvoidingView style={styles.input} 
-      behavior={Platform.OS === "ios" ? "padding" : null}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 5 : 0}
-      >
+      <View style={styles.input}>
         <TextInput
           keyboardType="email-address"
           style={styles.textInput}
@@ -160,30 +96,27 @@ export default function Login({ navigation }) {
           onChangeText={passwordHandler}
           value={password}
         />
-        <View style={styles.forgotPassword}>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("forgotPassword");
-            }}
-          >
-            <Text style={styles.forgotPasswordtext}>Esqueceu a senha?</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
+      </View>
       <View style={styles.viewBtn}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("forgotPassword");
+          }}
+          style={{ alignSelf: "flex-end" }}
+        >
+          <Text style={styles.forgotPasswordtext}>Esqueceu a senha?</Text>
+        </TouchableOpacity>
         <View style={styles.login}>
-          {!loading ? (
+          {loading ? (
+            <ActivityIndicator size="large" color={colors.light} />
+          ) : (
             <Button
               large
               type="white"
               title="ENTRAR"
               press={loginHandler}
-              disabled={
-                buttonDisabled || loading || loadingFace || loadingGoogle
-              }
+              disabled={buttonDisabled || loading}
             />
-          ) : (
-            <ActivityIndicator size="large" color={colors.light} />
           )}
         </View>
         <TouchableOpacity
@@ -192,38 +125,8 @@ export default function Login({ navigation }) {
             navigation.navigate("location");
           }}
         >
-          <Text style={styles.signupText}>Não tem uma conta?</Text>
+          <Text style={styles.signupText}>Criar Conta</Text>
         </TouchableOpacity>
-        <View style={styles.quickLogin}>
-          <View style={[{ backgroundColor: googleColor }, styles.viewGoogle]}>
-            {!loadingGoogle ? (
-              <TouchableOpacity
-                disabled={loading || loadingFace}
-                style={[styles.btnGoogle]}
-                onPress={loginHandlerGoogle}
-              >
-                <Icon type="antdesign" name={"google"} color={"white"} />
-              </TouchableOpacity>
-            ) : (
-              <ActivityIndicator size="large" color={colors.light} />
-            )}
-          </View>
-          <View
-            style={[{ backgroundColor: facebookColor }, styles.viewFacebook]}
-          >
-            {!loadingFace ? (
-              <TouchableOpacity
-                disabled={loading || loadingGoogle}
-                style={styles.btnFacebook}
-                onPress={loginHandlerFacebook}
-              >
-                <Icon type="font-awesome" name={"facebook"} color={"white"} />
-              </TouchableOpacity>
-            ) : (
-              <ActivityIndicator size="large" color={colors.light} />
-            )}
-          </View>
-        </View>
       </View>
     </View>
   );
