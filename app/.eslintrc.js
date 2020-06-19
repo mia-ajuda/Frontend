6218fede837ae2fddfe7fdf2eaf8a64c01ab2f41
Pedro Féo <pheofo@gmail.com>
2020-06-19 18:05:09 -0300

    config eslint

---
+++ app/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    'env': {
+        'es6': true,
+        'node': true,
+        'jest': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 11,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        'react-hooks'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-empty-function': 'off',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+    }
+};
