674d0e4d835a7f5c7cd14564149079eacc5eef2e
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-19 01:07:54 -0300

    Added terms of the modal to a new folder called docs

--- app/src/components/modals/conditionTermsModal/index.js
+++ app/src/components/modals/conditionTermsModal/index.js
@@ -1,22 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Modal, TouchableOpacity, ScrollView, Text, View } from "react-native";
 import { Icon } from "react-native-elements";
 import colors from "../../../../assets/styles/colorVariables";
 import styles from "./style";
 import Markdown from "react-native-markdown-display";
-import terms from "./terms";
-import PrivacyPolicyModal from "../privacyPolicyModal"
+import terms from "../../../docs/terms";
+import PrivacyPolicyModal from "../privacyPolicyModal";
 
 export default function TermsModal({ visible, setVisible }) {
   const [privacyModalVisible, setPrivacyModalVisible] = useState(false);
 
   return (
     <Modal
-    visible={visible}
-    onRequestClose={() => setVisible(false)}
-    animationType="slide"
+      visible={visible}
+      onRequestClose={() => setVisible(false)}
+      animationType="slide"
     >
-    <PrivacyPolicyModal visible={privacyModalVisible} setVisible={setPrivacyModalVisible}/>
+      <PrivacyPolicyModal
+        visible={privacyModalVisible}
+        setVisible={setPrivacyModalVisible}
+      />
       <TouchableOpacity
         style={styles.closeButton}
         onPress={() => setVisible(false)}
@@ -32,7 +35,10 @@ export default function TermsModal({ visible, setVisible }) {
         contentContainerStyle={{ margin: 20, paddingBottom: 20 }}
         showsVerticalScrollIndicator={false}
       >
-        <TouchableOpacity style={styles.privacyLink} onPress={() => setPrivacyModalVisible(true)}>
+        <TouchableOpacity
+          style={styles.privacyLink}
+          onPress={() => setPrivacyModalVisible(true)}
+        >
           <Text style={styles.privacyText}>
             Ir para política de privacidade
           </Text>
