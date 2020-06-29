2ae8ff7fdb171e57c0c4099f14817735fcf02c73
Gabriel Davi <gdavi.gd@gmail.com>
2020-06-29 18:51:33 -0300

    separeted icon options in bottomTab
    Co-authored-by: dudamelo82@gmail.com <dudamelo82>

--- app/src/Navigation/MainNavigation/BottomNavigator/index.js
+++ app/src/Navigation/MainNavigation/BottomNavigator/index.js
@@ -1,15 +1,13 @@
 import React, { useContext } from 'react';
-import { Image } from 'react-native';
 import { HelpContext } from '../../../store/contexts/helpContext';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-elements';
 import Notification from '../../../pages/notification';
 import MyRequestsNavigation from '../MyHelpRequestsNavigator';
 import MapNavigation from '../MapStackNavigator';
 import ProfileNavigation from '../ProfileNavigator';
 import MyOfferedHelpNavigation from '../MyOfferedHelpNavigator';
 import Splash from '../../../pages/splash';
-import navigationIcons from './navigationIcons';
+import navigationIconsConfig from './navigationsIcons.options';
 import navigationOptions from './BottomNavigator.options';
 
 const BottomNavigation = createBottomTabNavigator();
@@ -22,41 +20,8 @@ const BottomTab = () => {
         <BottomNavigation.Navigator
             tabBarOptions={navigationOptions}
             screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused }) => {
-                    if (route.name == 'main') {
-                        const activeIcon =
-                            navigationIcons[route.name].active.icon;
-                        const inactiveIcon =
-                            navigationIcons[route.name].inactive.icon;
-
-                        const activeIconStyle =
-                            navigationIcons[route.name].active.size;
-                        const inactiveIconStyle =
-                            navigationIcons[route.name].inactive.size;
-
-                        return (
-                            <Image
-                                source={focused ? activeIcon : inactiveIcon}
-                                style={
-                                    focused
-                                        ? activeIconStyle
-                                        : inactiveIconStyle
-                                }
-                            />
-                        );
-                    }
-                    const icon = navigationIcons[route.name].icon;
-                    const activeConfig = navigationIcons[route.name].active;
-                    const inactiveConfig = navigationIcons[route.name].inactive;
-                    return (
-                        <Icon
-                            {...(focused ? activeConfig : inactiveConfig)}
-                            size={20}
-                            name={icon}
-                            type="font-awesome"
-                        />
-                    );
-                },
+                tabBarIcon: ({ focused }) =>
+                    navigationIconsConfig(focused, route),
             })}
             initialRouteName="main">
             <BottomNavigation.Screen
