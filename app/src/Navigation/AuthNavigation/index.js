4380bbea6fb7b05860f8564cd5095ae271550d65
Gabriel Davi <gdavi.gd@gmail.com>
2020-07-01 18:03:40 -0300

    Moved splash to the main route page

--- app/src/Navigation/AuthNavigation/index.js
+++ app/src/Navigation/AuthNavigation/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../../pages/authPages/Login';
 import Location from '../../pages/authPages/Location';
@@ -8,16 +8,9 @@ import RiskGroup from '../../pages/authPages/RiskGroup';
 import Photo from '../../pages/authPages/Photo';
 import Address from '../../pages/authPages/Address';
 import ForgotPassword from '../../pages/authPages/ForgotPassword';
-import Splash from '../../pages/splash';
-import { UserContext } from '../../store/contexts/userContext';
 
 const AuthStack = createStackNavigator();
 const AuthRoutes = () => {
-    const { user } = useContext(UserContext);
-    if (user && user.showSplash) {
-        return <Splash />;
-    }
-
     return (
         <AuthStack.Navigator
             initialRouteName="login"
--- app/src/Navigation/index.js
+++ app/src/Navigation/index.js
@@ -3,10 +3,17 @@ import BottomTab from './MainNavigation/BottomNavigator';
 import AuthRoutes from './AuthNavigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { UserContext } from '../store/contexts/userContext';
+import Splash from '../pages/splash';
 
 const Routes = () => {
     const { user } = useContext(UserContext);
-    const isUserAuthenticated = user.id;
+    const loadingUserInformation = user && user.showSplash;
+    const isUserAuthenticated = user._id;
+
+    if (loadingUserInformation) {
+        return <Splash />;
+    }
+
     return (
         <NavigationContainer>
             {isUserAuthenticated ? <BottomTab /> : <AuthRoutes />}
