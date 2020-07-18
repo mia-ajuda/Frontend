c215e6c45ff021441b33c90102c07afd0061cfcb
Gabriel Davi <gdavi.gd@gmail.com>
2020-07-17 23:09:31 -0300

    New app version submited to the app store

--- app/app.config.js
+++ app/app.config.js
@@ -1,25 +1,26 @@
-import { MAPS_API_KEY } from "react-native-dotenv";
+import { MAPS_API_KEY } from 'react-native-dotenv';
 
 export default ({ config }) => {
-  return {
-    ...config,
-    android: {
-      package: "com.unb.miaajuda",
-      permissions: [
-        "ACCESS_COARSE_LOCATION",
-        "ACCESS_FINE_LOCATION",
-        "CAMERA",
-        "MANAGE_DOCUMENTS",
-        "READ_EXTERNAL_STORAGE",
-        "READ_PHONE_STATE",
-        "WRITE_EXTERNAL_STORAGE",
-      ],
-      googleServicesFile: "./google-services.json",
-      config: {
-        googleMaps: {
-          apiKey: MAPS_API_KEY,
+    return {
+        ...config,
+        android: {
+            package: 'com.unb.miaajuda',
+            permissions: [
+                'ACCESS_COARSE_LOCATION',
+                'ACCESS_FINE_LOCATION',
+                'CAMERA',
+                'MANAGE_DOCUMENTS',
+                'READ_EXTERNAL_STORAGE',
+                'READ_PHONE_STATE',
+                'WRITE_EXTERNAL_STORAGE',
+            ],
+            googleServicesFile: './google-services.json',
+            config: {
+                googleMaps: {
+                    apiKey: MAPS_API_KEY,
+                },
+            },
+            versionCode: 2,
         },
-      },
-    },
-  };
+    };
 };
