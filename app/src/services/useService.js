3e57388d7537c2373c6350e262952e88cf03f7ae
Gabriel Davi <gdavi.gd@gmail.com>
2020-07-31 16:56:02 -0300

    removed console.log

--- app/src/services/useService.js
+++ app/src/services/useService.js
@@ -5,7 +5,6 @@ export default async function useService(service, functionName, params = []) {
         let functionReturn = await service[functionName](...params);
         return functionReturn;
     } catch (error) {
-        console.log(error);
         if (error.code == 'auth/email-not-verified') {
             alertMessageEmailVerification(error.message);
         } else {
