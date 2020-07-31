6380e98d2a0b029b4884b10c36a16dc07b882e1e
PedroLimass <phlimas@outlook.com>
2020-07-31 17:15:49 -0300

    Afetuado o encurtamento dos nomes em algumas das HelpPages

--- app/src/pages/HelpPages/MyRequestHelpDescrition/index.js
+++ app/src/pages/HelpPages/MyRequestHelpDescrition/index.js
@@ -43,7 +43,7 @@ export default function HelpDescription({ route, navigation }) {
             />
             <View style={styles.infoTextView}>
                 <Text style={[styles.infoText, styles.infoTextFont]}>
-                    {help.user.name}
+                    {help.user.name.split(' ').slice(0, 2).join(' ')}
                 </Text>
                 <Text style={styles.infoText}>
                     <Text style={styles.infoTextFont}>Idade: </Text>
