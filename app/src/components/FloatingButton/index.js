3811f9fabaacd3c076031d8e411136f3d354beac
Gabriel Davi <gdavi.gd@gmail.com>
2020-08-03 18:46:29 -0300

    Create the floating buttons

--- app/src/components/FloatingButton/index.js
+++ app/src/components/FloatingButton/index.js
@@ -1,98 +1,153 @@
 import React, { useState } from 'react';
 import Animated, { Easing, timing } from 'react-native-reanimated';
-import { Text, TouchableOpacity } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 import styles from './styles';
+import colors from '../../../assets/styles/colorVariables';
 
 const buttonsTransleY = new Animated.Value(0);
+const BUTTON_MAX_HEIGHT = 120;
+const BUTTON_MIN_HEIGHT = 0;
+
+const AnimatedButton = Animated.createAnimatedComponent(TouchableOpacity);
+
 export default function FloatingButtons() {
+    const navigation = useNavigation();
     const [isButtonsShown, setButtonsShown] = useState(false);
 
     const toggleButtonsVisibility = () => {
         if (isButtonsShown) {
             hideButtons();
-            setButtonsShown(false);
         } else {
             showButtons();
-            setButtonsShown(true);
         }
     };
+
+    const navigateToCreateHelpPage = () => {
+        navigation.navigate('createHelp');
+        hideButtons();
+    };
+
     const showButtons = () => {
         const showButtonsConfig = {
             duration: 150,
-            toValue: 100,
+            toValue: BUTTON_MAX_HEIGHT,
             easing: Easing.inOut(Easing.ease),
         };
         timing(buttonsTransleY, showButtonsConfig).start();
+        setButtonsShown(true);
     };
 
     const hideButtons = () => {
         const hideButtonsConfig = {
             duration: 150,
-            toValue: 0,
+            toValue: BUTTON_MIN_HEIGHT,
             easing: Easing.inOut(Easing.ease),
         };
         timing(buttonsTransleY, hideButtonsConfig).start();
+        setButtonsShown(false);
     };
+
+    const renderPlusButton = () => (
+        <Animated.View
+            style={[
+                {
+                    transform: [
+                        {
+                            rotate: buttonsTransleY.interpolate({
+                                inputRange: [
+                                    BUTTON_MIN_HEIGHT,
+                                    BUTTON_MAX_HEIGHT,
+                                ],
+                                outputRange: [0, 0.8],
+                            }),
+                        },
+                    ],
+                },
+                styles.plusButtonView,
+            ]}>
+            <TouchableOpacity
+                style={styles.plusButton}
+                activeOpacity={1}
+                onPress={toggleButtonsVisibility}>
+                <FontAwesome name="plus" size={30} color="#fff" />
+            </TouchableOpacity>
+        </Animated.View>
+    );
+
+    const renderRequestHelpButton = () => (
+        <AnimatedButton
+            onPress={navigateToCreateHelpPage}
+            style={[
+                {
+                    transform: [
+                        {
+                            translateY: buttonsTransleY.interpolate({
+                                inputRange: [
+                                    BUTTON_MIN_HEIGHT,
+                                    BUTTON_MAX_HEIGHT,
+                                ],
+                                outputRange: [0, -BUTTON_MAX_HEIGHT / 2],
+                            }),
+                        },
+                    ],
+                },
+                styles.helpButtonView,
+            ]}>
+            {isButtonsShown && (
+                <Text style={styles.helpButtonText}>Pedir ajuda</Text>
+            )}
+            <View
+                style={[
+                    { transform: [{ rotate: '-15deg' }] },
+                    styles.helpButton,
+                ]}>
+                <FontAwesome
+                    name="exclamation"
+                    size={50}
+                    color={colors.primary}
+                />
+            </View>
+        </AnimatedButton>
+    );
+    const renderOfferButton = () => (
+        <AnimatedButton
+            style={[
+                {
+                    transform: [
+                        {
+                            translateY: buttonsTransleY.interpolate({
+                                inputRange: [
+                                    BUTTON_MIN_HEIGHT,
+                                    BUTTON_MAX_HEIGHT,
+                                ],
+                                outputRange: [0, -BUTTON_MAX_HEIGHT],
+                            }),
+                        },
+                    ],
+                },
+                styles.helpButtonView,
+            ]}>
+            {isButtonsShown && (
+                <Text style={styles.helpButtonText}>oferecer ajuda</Text>
+            )}
+
+            <View style={styles.helpButton}>
+                <FontAwesome5
+                    name="hand-holding-heart"
+                    size={30}
+                    color={colors.primary}
+                />
+            </View>
+        </AnimatedButton>
+    );
 
     return (
         <>
-            <Animated.View
-                style={[
-                    {
-                        transform: [
-                            {
-                                rotate: buttonsTransleY.interpolate({
-                                    inputRange: [0, 100],
-                                    outputRange: [0, 1],
-                                }),
-                            },
-                        ],
-                    },
-                    styles.plusButtonView,
-                ]}>
-                <TouchableOpacity
-                    style={styles.plusButton}
-                    onPress={toggleButtonsVisibility}>
-                    <FontAwesome name="plus" size={30} color="#fff" />
-                </TouchableOpacity>
-            </Animated.View>
-
-            <Animated.View
-                style={[
-                    {
-                        transform: [
-                            {
-                                translateX: buttonsTransleY.interpolate({
-                                    inputRange: [0, 100],
-                                    outputRange: [0, -100],
-                                }),
-                            },
-                        ],
-                    },
-                    styles.helpButtonView,
-                ]}>
-                <Text>Teste</Text>
-                <TouchableOpacity style={styles.helpButton} />
-            </Animated.View>
-
-            <Animated.View
-                style={[
-                    {
-                        transform: [
-                            {
-                                translateX: buttonsTransleY.interpolate({
-                                    inputRange: [0, 100],
-                                    outputRange: [0, -50],
-                                }),
-                            },
-                        ],
-                    },
-                    styles.helpButtonView,
-                ]}>
-                <Text>Teste</Text>
-                <TouchableOpacity style={styles.helpButton} />
-            </Animated.View>
+            {renderPlusButton()}
+            {renderOfferButton()}
+            {renderRequestHelpButton()}
         </>
     );
 }
--- app/src/components/FloatingButton/styles.js
+++ app/src/components/FloatingButton/styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../../assets/styles/colorVariables';
 
 const styles = StyleSheet.create({
     plusButtonView: {
@@ -11,14 +12,14 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
         borderRadius: 25,
-        backgroundColor: '#4B8AB9',
+        backgroundColor: colors.primary,
 
         alignItems: 'center',
         justifyContent: 'center',
     },
     helpButtonView: {
         bottom: 55,
-        right: 20,
+        right: 25,
         position: 'absolute',
         zIndex: 2,
         alignItems: 'center',
@@ -29,6 +30,15 @@ const styles = StyleSheet.create({
         height: 40,
         backgroundColor: '#fff',
         borderRadius: 20,
+        marginLeft: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    helpButtonText: {
+        backgroundColor: '#fff',
+        paddingHorizontal: 5,
+        paddingVertical: 3,
+        fontFamily: 'montserrat-semibold',
     },
 });
 
