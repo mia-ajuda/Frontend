116ef0388ed3ef2764e3a019d89a56a45ab8ab97
Gabriel Davi <gdavi.gd@gmail.com>
2020-08-05 23:11:54 -0300

    Adjust category description title size

---
+++ app/src/components/modals/category/CategorySelector/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const CategorySelector = () => {
+    return <View />;
+};
+
+export default CategorySelector;
