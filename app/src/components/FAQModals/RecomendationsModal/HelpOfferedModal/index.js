e6888902eaa7cf382298efe1adcd42fb7624b8c5
dudamelo82 <43159230+dudamelo82@users.noreply.github.com>
2020-08-08 20:51:53 -0300

    Componentes criados da página do FAQ

---
+++ app/src/components/FAQModals/RecomendationsModal/HelpOfferedModal/index.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Modal, ScrollView,TouchableOpacity, View, ImageBackground } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Container from '../../../Container';
+import colors from '../../../../../../assets/styles/colorVariables';
+import styles from './styles';
+
+
+export default function HelpOfferedRecomendations({ visible, setVisible }) {
+
+  const acceptHelpRequestRecomendationsDescription = `
+  ##1 Lembre-se que você está lidando com pessoas que precisam de ajuda, logo, tenha paciência, seja prestativo e gentil;
+
+  ##2 Ao entrar em contato com o ajudado, considere que o mesmo pode estar em alguma situação de risco, por isso, não peça, e nem ofereça, informações adicionais além das apresentadas no app;
+
+  ##3 Recomendamos, caso seja necessário um encontro, que este ocorra em um local público, e de forma segura.
+  `;
+
+  const createHelpOfferedRecomendationsDescriptions = `
+  ##1 Se um objeto for ofertado, certifique-se de que o mesmo está em boas condições de uso;
+
+  ##2 Certifique-se, ao escolher um ajudado, de que este realmente precisa da ajuda;
+
+  ##3 Caso seja ofertado um serviço ( apoio social, psicológico ou físico, transporte de emergência e pequenos serviços) lembre-se de realizá-lo da melhor forma possível, e sempre com paciência e prestatividade.
+ `;
+
+  const HelpOfferedRecomendations =[
+    {id:'1',
+    title:'Recomendações importantes para aceitar um pedido de ajuda',
+    description:acceptHelpRequestRecomendationsDescription},
+    {id:'2',
+    title:'Recomendações importante para oferecer ajuda',
+    description:createHelpOfferedRecomendationsDescriptions}
+  ];
+
+
+  return (
+    
+        <Modal 
+        visible={visible}
+        transparent
+        onRequestClose={() => setVisible(false)}
+        animationType="fade"
+    >
+        <View style={styles.modalContainer}>
+        <ImageBackground
+                source={require('../../../../../images/offerHelpHand.png')}
+                style={styles.image}>
+                <Container>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setVisible(false);
+                        }}
+                        style={styles.icon}>
+                        <Icon
+                            name="times-circle"
+                            type="font-awesome"
+                            color={colors.primary}
+                            size={35}
+                        />
+                        </TouchableOpacity>
+
+              <View style={styles.modalContent}>
+              <ScrollView indicatorStyle="white">
+                            {HelpOfferedRecomendations.map((item) => (
+                                <View key={item.id}>
+                                    <Text style={styles.title}>
+                                        {item.title}
+                                    </Text>
+                                    <Text style={styles.description}>
+                                        {item.description}
+                                    </Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                </View>      
+            
+            </Container>
+            </ImageBackground>
+        </View>
+    </Modal>
+
+    
+    
+
+
+  );
+}
---
+++ app/src/components/FAQModals/RecomendationsModal/HelpRequestModal/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Modal, ScrollView,TouchableOpacity, View, ImageBackground } from 'react-native';
+import { Icon } from 'react-native-elements';
+import Container from '../../../Container';
+import colors from '../../../../../../assets/styles/colorVariables';
+import styles from './styles';
+
+
+export default function HelpRequestRecomendations({ visible, setVisible }) {
+
+  const acceptHelpOfferRecomendationsDescription = `
+  ##1 Verifique se a oferta está de acordo com a sua necessidade, caso contrário deixe a oportunidade para alguém que realmente precise;
+
+ ##2 Caso a oferta se adeque à sua situação, aceite-a e aguarde o contato do responsável por ela;
+
+ ##3 Ao receber a ajuda, lembre-se de agradecer ao voluntário.
+
+  `;
+
+  const createHelpRequestRecomendationsDescriptions = `
+  ##1 Lembre-se sempre que você está lidando com outras pessoas, então não peça por coisas impossíveis, nem por dinheiro pois é importante que todos possam ajudar;
+
+  ##2 Ao receber voluntários para te ajudar, nunca esqueça de ser educado e gentil, pois eles estão tentando fazer o possível para atender à sua solicitação na situação pela qual você está passando;
+
+  ##3 Recomendamos, caso seja necessário um encontro, que este ocorra em um local público, e de forma segura.
+  
+ `;
+
+  const HelpOfferedRecomendations =[
+    {id:'1',
+    title:'Recomendações importantes para se interessar por alguma oferta de ajuda',
+    description:acceptHelpOfferRecomendationsDescription},
+    {id:'2',
+    title:'Recomendações importantes para criar de pedidos de ajuda ',
+    description:createHelpRequestRecomendationsDescriptions}
+  ];
+
+
+  return (
+    
+        <Modal 
+        visible={visible}
+        transparent
+        onRequestClose={() => setVisible(false)}
+        animationType="fade"
+    >
+        <View style={styles.modalContainer}>
+        <ImageBackground
+                source={require('../../../../../images/helpRequestExclamation.png')}
+                style={styles.image}>
+                <Container>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setVisible(false);
+                        }}
+                        style={styles.icon}>
+                        <Icon
+                            name="times-circle"
+                            type="font-awesome"
+                            color={colors.primary}
+                            size={35}
+                        />
+                        </TouchableOpacity>
+
+              <View style={styles.modalContent}>
+              <ScrollView indicatorStyle="white">
+                            {HelpOfferedRecomendations.map((item) => (
+                                <View key={item.id}>
+                                    <Text style={styles.title}>
+                                        {item.title}
+                                    </Text>
+                                    <Text style={styles.description}>
+                                        {item.description}
+                                    </Text>
+                                </View>
+                            ))}
+                        </ScrollView>
+                </View>      
+            
+            </Container>
+            </ImageBackground>
+        </View>
+    </Modal>
+  );
+}
