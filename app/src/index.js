107bfda602d3dca38ccffc9d6df04e0d96c53057
Danillo <danillosouza1704@gmail.com>
2020-08-11 13:20:46 -0300

    Added Campaign Context to Root

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Routes from './Navigation';
 import { UserContextProvider } from './store/contexts/userContext';
 import HelpContextProvider from './store/contexts/helpContext';
+import CampaignContextProvider from './store/contexts/campaignContext';
 import CategoryContextProvider from './store/contexts/categoryContext';
 import DeviceInfoProvider from './store/contexts/deviceInformationContext';
 
@@ -10,9 +11,11 @@ export default function Root() {
         <DeviceInfoProvider>
             <UserContextProvider>
                 <CategoryContextProvider>
-                    <HelpContextProvider>
-                        <Routes />
-                    </HelpContextProvider>
+                    <CampaignContextProvider>
+                        <HelpContextProvider>
+                            <Routes />
+                        </HelpContextProvider>
+                    </CampaignContextProvider>
                 </CategoryContextProvider>
             </UserContextProvider>
         </DeviceInfoProvider>
