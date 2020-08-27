51a2ed0ecd05575af5dbc73c3ae56f3b395de6bc
Danillo <danillosouza1704@gmail.com>
2020-08-26 22:03:30 -0300

    Fixed problem with statusbar

--- app/src/pages/IntroSlides/index.js
+++ app/src/pages/IntroSlides/index.js
@@ -1,4 +1,4 @@
-import { Image, AsyncStorage } from 'react-native';
+import { Image, AsyncStorage, View, StatusBar } from 'react-native';
 import React from 'react';
 import { Button } from 'react-native-elements';
 
@@ -63,71 +63,75 @@ export default function IntroSlides() {
         />
     );
     return (
-        <Onboarding
-            //onDone={() => console.log('done')}
-            titleStyles={{
-                color: '#4B8AB9',
-                fontFamily: 'montserrat-semibold',
-            }}
-            subTitleStyles={{
-                fontFamily: 'montserrat-semibold',
-            }}
-            NextButtonComponent={Next}
-            SkipButtonComponent={Skip}
-            DoneButtonComponent={Done}
-            onDone={completeIntroSlide}
-            pages={[
-                {
-                    title: 'Precisando de Ajuda ou querendo oferecer?',
-                    backgroundColor: '#fff',
-                    image: (
-                        <Image
-                            source={require('../../../assets/images/TutorialImages/tela01.png')}
-                        />
-                    ),
-                    subtitle:
-                        'O Mia Ajuda possibilita que você solicite e oferte ajuda. Desde uma simples conversa até itens de necessidade básica.',
-                },
-                {
-                    backgroundColor: '#fff',
-                    image: (
-                        <Image
-                            source={require('../../../assets/images/TutorialImages/tela02.png')}
-                        />
-                    ),
-                    title: 'Não sabe como ajudar?',
-                    subtitle:
-                        'Conte uma história! Cante para alguém! Encontre um amigo para conversar! Doe um alimento! Colabore como queira! O importante é ajudar!',
-                },
-                {
-                    backgroundColor: '#fff',
-                    image: (
-                        <Image
-                            source={require('../../../assets/images/TutorialImages/tela03.png')}
-                        />
-                    ),
-                    title: 'Você é uma ONG?',
-                    subtitle:
-                        'Temos uma parte especial para ONGs. Venha ser um apoiador e torne o mundo cada vez melhor por meio da solidariedade!',
-                },
-                {
-                    backgroundColor: '#fff',
-                    image: (
-                        <Image
-                            source={require('../../../assets/images/TutorialImages/tela04.png')}
-                        />
-                    ),
-                    title:
-                        'Ajude pessoas perto de você é espalhe amor pelo mundo',
-                    subtitle: '',
-                    titleStyles: {
-                        color: 'rgba(0,0,0, 0.7)',
-                        fontSize: 20,
-                        marginTop: -50,
+        <View style={{ flex: 1 }}>
+            <StatusBar backgroundColor="white" barStyle="dark-content" />
+            <Onboarding
+                //onDone={() => console.log('done')}
+                titleStyles={{
+                    color: '#4B8AB9',
+                    fontFamily: 'montserrat-semibold',
+                }}
+                subTitleStyles={{
+                    fontFamily: 'montserrat-semibold',
+                }}
+                controlStatusBar={false}
+                NextButtonComponent={Next}
+                SkipButtonComponent={Skip}
+                DoneButtonComponent={Done}
+                onDone={completeIntroSlide}
+                pages={[
+                    {
+                        title: 'Precisando de Ajuda ou querendo oferecer?',
+                        backgroundColor: '#fff',
+                        image: (
+                            <Image
+                                source={require('../../../assets/images/TutorialImages/tela01.png')}
+                            />
+                        ),
+                        subtitle:
+                            'O Mia Ajuda possibilita que você solicite e oferte ajuda. Desde uma simples conversa até itens de necessidade básica.',
                     },
-                },
-            ]}
-        />
+                    {
+                        backgroundColor: '#fff',
+                        image: (
+                            <Image
+                                source={require('../../../assets/images/TutorialImages/tela02.png')}
+                            />
+                        ),
+                        title: 'Não sabe como ajudar?',
+                        subtitle:
+                            'Conte uma história! Cante para alguém! Encontre um amigo para conversar! Doe um alimento! Colabore como queira! O importante é ajudar!',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: (
+                            <Image
+                                source={require('../../../assets/images/TutorialImages/tela03.png')}
+                            />
+                        ),
+                        title: 'Você é uma ONG?',
+                        subtitle:
+                            'Temos uma parte especial para ONGs. Venha ser um apoiador e torne o mundo cada vez melhor por meio da solidariedade!',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: (
+                            <Image
+                                source={require('../../../assets/images/TutorialImages/tela04.png')}
+                            />
+                        ),
+                        title:
+                            'Ajude pessoas perto de você é espalhe amor pelo mundo',
+                        subtitle: '',
+                        titleStyles: {
+                            color: 'rgba(0,0,0, 0.7)',
+                            fontSize: 20,
+                            marginTop: -50,
+                        },
+                    },
+                ]}
+            />
+        </View>
     );
 }
 ///
