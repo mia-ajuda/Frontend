e085aea6f3a2cc89c164d436d99c7c6c33ea515c
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-07-28 17:41:58 -0300

    [#182] - Change configuration from Stack navigation to always return to the initial page
    Co-authored-by: Brian Pina <brian.pina2001@gmail.com>

--- app/src/docs/FAQ/HowToCreateHelp.js
+++ app/src/docs/FAQ/HowToCreateHelp.js
@@ -1,15 +1,15 @@
 const HowToCreateHelpRequestModal = `
-##1 Para pedir uma ajuda clique no ícone;
+1. Para pedir uma ajuda clique no ícone;
 
-##2 Preencha os dados da página de pedidos: título, categoria e descrição;
+2. Preencha os dados da página de pedidos: título, categoria e descrição;
 
-##3 Depois de criado o seu pedido, é só esperar que alguém ofereça ajuda;
+3. Depois de criado o seu pedido, é só esperar que alguém ofereça ajuda;
 
-##4 Quando alguém oferecer ajuda, uma notificação será enviado pelo app;
+4. Quando alguém oferecer ajuda, uma notificação será enviado pelo app;
 
-##5 O contato entre você e o voluntário a lhe ajudar será feito através do telefone ou alguma aplicativo de conversa que vocês utilizem (Exemplo: WhatsApp, Facebook);
+5. O contato entre você e o voluntário a lhe ajudar será feito através do telefone ou alguma aplicativo de conversa que vocês utilizem (Exemplo: WhatsApp, Facebook);
 
-##6 Depois que você recebeu a sua ajuda, é só entrar no app e finalizar o seu pedido.
+6. Depois que você recebeu a sua ajuda, é só entrar no app e finalizar o seu pedido.
 `;
 
 const createHelpRecomendations = [
--- app/src/docs/FAQ/HowToOfferHelp.js
+++ app/src/docs/FAQ/HowToOfferHelp.js
@@ -1,17 +1,17 @@
 const HowToOfferHelpRecomendations = `
-##1 Para ofertar uma ajuda clique no ícone de criar oferta ;
+1. Para ofertar uma ajuda clique no ícone de criar oferta ;
 
-##2 Preencha os dados da página de pedidos: título, categoria e descrição;
+2. Preencha os dados da página de pedidos: título, categoria e descrição;
 
-##3 Depois de efetuada a oferta do seu pedido, é só aguardar que uma ou mais pessoas se interessarem pela sua ajuda;
+3. Depois de efetuada a oferta do seu pedido, é só aguardar que uma ou mais pessoas se interessarem pela sua ajuda;
 
-##4 A lista de interessados aparecerá no histórico da sua oferta e uma notificação será enviada pelo app;
+4. A lista de interessados aparecerá no histórico da sua oferta e uma notificação será enviada pelo app;
 
-##5 Você pode aceitar ajudar uma ou mais pessoas;
+5. Você pode aceitar ajudar uma ou mais pessoas;
 
-##6 O contato entre você e o interessado que você irá ajudar será feito através do telefone ou alguma aplicativo de conversa que vocês utilizem (Exemplo: WhatsApp, Facebook);
+6. O contato entre você e o interessado que você irá ajudar será feito através do telefone ou alguma aplicativo de conversa que vocês utilizem (Exemplo: WhatsApp, Facebook);
 
-##7 Depois de ajudar os interessados, é só entrar no app e finalizar a sua oferta.`;
+7. Depois de ajudar os interessados, é só entrar no app e finalizar a sua oferta.`;
 
 const offerHelpRecomendations = [
     {
