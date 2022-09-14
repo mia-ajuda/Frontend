6f270ade74c44acd393f3261d465aaf40783242f
Paulo Gonçalves Lima <18203100+PauloGoncalvesLima@users.noreply.github.com>
2022-09-14 18:00:13 -0300

    Update emailValidator.js

--- app/src/utils/emailValidator.js
+++ app/src/utils/emailValidator.js
@@ -1,5 +1,5 @@
 const validationEmail = (email) => {
-    const expression = /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const expression = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return expression.test(String(email).toLowerCase());
 };
