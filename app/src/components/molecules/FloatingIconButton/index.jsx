607fd24c56c8fa7012e2fd68ff4b176feac33d83
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-02-28 23:58:13 -0300

    feat: create floating circle button

---
+++ app/src/components/molecules/FloatingIconButton/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+import { CircleIconButton } from '../../atoms/CircleIconButton';
+
+export const FloatingIconButton = ({
+    position = 'left',
+    iconName,
+    onPress,
+}) => {
+    const convertPosition = position === 'left' ? 'left-2' : 'right-2';
+    return (
+        <View className={`absolute top-2 ${convertPosition}`}>
+            <CircleIconButton icon={iconName} onPress={onPress} />
+        </View>
+    );
+};
