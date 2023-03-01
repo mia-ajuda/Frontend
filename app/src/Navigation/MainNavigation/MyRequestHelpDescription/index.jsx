4dbb427f23613be03a8544780d60d808a58851e6
sudjoao <isudjoao@gmail.com>
2023-03-01 20:16:29 -0300

    chore: change navigation files to jsx

--- app/src/Navigation/MainNavigation/MyRequestHelpDescription/index.jsx
+++ app/src/Navigation/MainNavigation/MyRequestHelpDescription/index.jsx
@@ -8,28 +8,31 @@ import HelpersInfo from '../../../components/HelpersInfo';
 const TopTab = createMaterialTopTabNavigator();
 export const MyRequestHelpDescription = ({ route, navigation }) => {
     const { help } = route.params;
-    return help.helperId ? (
-        <TopTab.Navigator
-            initialRouteName="InfosPedido"
-            tabBarOptions={tabTopBarOptions}
-        >
-            <TopTab.Screen name="Ajudante">
-                {() => (
-                    <HelpersInfo
-                        userId={help.helperId}
-                        title="Ajudante escolhido"
-                    />
-                )}
-            </TopTab.Screen>
-            <TopTab.Screen
-                initialParams={{
-                    help: help,
-                }}
-                name="Informação do Pedido"
-                component={MyRequestDescription}
-            />
-        </TopTab.Navigator>
-    ) : (
-        <MyRequestDescription route={route} navigation={navigation} />
-    );
+    const hasHelper = help.helperId;
+    return hasHelper ? RequestWithHelpScreen() :
+        <MyRequestDescription route={route} navigation={navigation} />;
 };
+
+const RequestWithHelpScreen = () => (
+    <TopTab.Navigator
+        initialRouteName="InfosPedido"
+        tabBarOptions={tabTopBarOptions}
+    >
+
+        <TopTab.Screen name="Ajudante">
+            {() => (
+                <HelpersInfo
+                    userId={help.helperId}
+                    title="Ajudante escolhido"
+                />
+            )}
+        </TopTab.Screen>
+        <TopTab.Screen
+            initialParams={{
+                help: help,
+            }}
+            name="Informação do Pedido"
+            component={MyRequestDescription}
+        />
+    </TopTab.Navigator>
+)
