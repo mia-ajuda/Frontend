a6a89f75ab2f3917ec8d159fa7248cf82491bcd7
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-03-04 05:07:47 -0300

    feat: create atom rounded full button

--- app/src/components/UserItem/index.jsx
+++ app/src/components/UserItem/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { TouchableOpacity, View, Image, Text } from 'react-native';
 import { untilTwoLastNames } from '../../utils/shortenName';
+import { RoundedFullButton } from '../atoms/RoundedFullButton';
 
-const UserItem = ({ user }) => {
+const UserItem = ({ user, shouldRenderRoundedFullButton = false, onPress }) => {
+    const renderRoundedFullButton = () => {
+        return (
+            shouldRenderRoundedFullButton && (
+                <View className="absolute right-0 z-10 transform -translate-x-1/2 -translate-y-1/2">
+                    <RoundedFullButton text="Aceitar" onPress={onPress} />
+                </View>
+            )
+        );
+    };
+
     return (
-        <View className="border-[#D2D2D2] border-b-[0.5px] mb-4">
-            <TouchableOpacity className="flex-row space-x-3 h-16">
+        <View className="border-[#D2D2D2] border-b-[0.5px] justify-center py-2">
+            {renderRoundedFullButton()}
+            <TouchableOpacity className="flex-row space-x-3 h-16 w-full items-center">
                 <Image
                     className="w-[48] h-[48] rounded-full"
                     source={{
