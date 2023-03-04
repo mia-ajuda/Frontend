dd7673c336bffb6878463a06e1bc3813614fb91a
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-03-04 05:12:34 -0300

    feat: create atom for default button

---
+++ app/src/components/atoms/DefaultButton/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+import colors from '../../../../assets/styles/colorVariables';
+
+export const DefaultButton = ({ title, onPress, disabled }) => {
+    return (
+        <Pressable
+            onPress={onPress}
+            className="w-full py-3 rounded-md bg-primary"
+            disabled={disabled}
+            android_ripple={{ color: colors.primaryContrast }}
+        >
+            <Text className="text-center text-light font-[montserrat-semibold] text-lg">
+                {title}
+            </Text>
+        </Pressable>
+    );
+};
