e6f883add5b53512f43225f33fc74c33f93aff51
sudjoao <isudjoao@gmail.com>
2023-03-04 06:39:36 -0300

    chore: limit view width and add maxOfLines in usercard

--- app/src/components/molecules/UserCard/index.jsx
+++ app/src/components/molecules/UserCard/index.jsx
@@ -5,6 +5,7 @@ import { TouchableOpacity } from 'react-native-gesture-handler'
 import callService from '../../../services/callService'
 import { styles } from './styles'
 import SessionService from '../../../services/Session';
+import shortenName from '../../../utils/shortenName'
 
 export const UserCard = ({ name, email, photo }) => {
     const handleLogout = async () => {
@@ -19,8 +20,8 @@ export const UserCard = ({ name, email, photo }) => {
                     style={styles.userPhoto}
                 />
                 <View>
-                    <Text style={styles.userName}>{name}</Text>
-                    <Text style={styles.userEmail}>{email}</Text>
+                    <Text style={styles.userName} numberOfLines={1}>{shortenName(name)}</Text>
+                    <Text style={styles.userEmail} numberOfLines={1}>{email}</Text>
                 </View>
             </View>
             <TouchableOpacity onPress={handleLogout}>
