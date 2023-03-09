a74490060ea0d8ca993332f5ff95bfec5d2d5b56
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-03-09 01:27:47 -0300

    feat: add possible custom styles to default btn

--- app/src/components/atoms/DefaultButton/index.jsx
+++ app/src/components/atoms/DefaultButton/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Pressable, Text } from 'react-native';
 import colors from '../../../../assets/styles/colorVariables';
 
-export const DefaultButton = ({ title, onPress, disabled }) => {
+export const DefaultButton = ({
+    title,
+    onPress,
+    disabled,
+    customStyle = '',
+}) => {
     return (
         <Pressable
             onPress={onPress}
-            className="w-full py-3 rounded-md bg-primary"
+            className={`w-full py-3 rounded-md bg-primary ${customStyle}`}
             disabled={disabled}
             android_ripple={{ color: colors.primaryContrast }}
         >
