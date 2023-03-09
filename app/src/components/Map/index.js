68354909bc257a21c60e12dd0cd6d2db29802e03
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-03-09 01:29:33 -0300

    feat: add more configurations to map view

--- app/src/components/Map/index.js
+++ app/src/components/Map/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import MapView from 'react-native-maps';
 import mapstyle from '../../../assets/styles/mapstyle';
 
 export default function Map({ children, initialRegion, region, ...rest }) {
+    const mapRef = useRef(null);
+
     return (
         <MapView
+            ref={mapRef}
+            provider="google"
             initialRegion={initialRegion}
-            className="w-full h-full"
             region={region}
+            className="w-full h-full"
+            showsUserLocation={true}
             onPress={() =>
                 rest.setHelpListVisible && rest.setHelpListVisible(false)
             }
