8c8b9c66f6988b846a176078cdb694962f5a7280
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-03-09 01:36:05 -0300

    fix: fix styles

--- app/src/pages/ActivitiesPages/MyOfferedHelp/MyOfferHelpDescription/styles.js
+++ app/src/pages/ActivitiesPages/MyOfferedHelp/MyOfferHelpDescription/styles.js
@@ -7,15 +7,6 @@ const styles = StyleSheet.create({
     scrollViewOffer: {
         flexGrow: 1,
     },
-    container: {
-        padding: 0,
-    },
-    viewBackdrop: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0,0,0,.6)',
-    },
     waitingText: {
         ...fonts.subtitle,
         textAlign: 'center',
@@ -24,6 +15,7 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         padding: 5,
         color: 'white',
+        marginTop: 24,
     },
     loadingContainer: {
         height: '100%',
