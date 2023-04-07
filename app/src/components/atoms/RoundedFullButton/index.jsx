88e8547bb470fe8c7738bff6972c8577d2256803
sudjoao <isudjoao@gmail.com>
2023-04-07 18:39:49 -0300

    feat: update rounded full button props and logic

--- app/src/components/atoms/RoundedFullButton/index.jsx
+++ app/src/components/atoms/RoundedFullButton/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-export const RoundedFullButton = ({ text, onPress }) => {
+export const RoundedFullButton = ({
+    text,
+    onPress,
+    variant = 'primary',
+    disabled = false,
+    width,
+}) => {
+    const variantStyles = {
+        primary: 'bg-primary text-white',
+        secondary: 'bg-white text-primary border border-1 border-primary',
+        danger: 'bg-danger text-white',
+        warning: 'bg-warning text-black',
+    };
+
+    const selectedVariant = variantStyles[variant];
+
     return (
-        <TouchableOpacity onPress={onPress}>
-            <Text className="bg-primary px-7 py-2 rounded-full text-white text-sm">
+        <TouchableOpacity onPress={onPress} disabled={disabled}>
+            <Text
+                className={`${selectedVariant} ${width} px-7 py-2 rounded-full text-sm font-semibold text-center`}
+            >
                 {text}
             </Text>
         </TouchableOpacity>
