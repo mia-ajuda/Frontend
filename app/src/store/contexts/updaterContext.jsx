0c0ecab4ba5019b54bd5f3c1987faa63aa71263a
sudjoao <isudjoao@gmail.com>
2023-04-07 21:25:17 -0300

    chore: add usememo in updater context

--- app/src/store/contexts/updaterContext.jsx
+++ app/src/store/contexts/updaterContext.jsx
@@ -1,11 +1,18 @@
-import { createContext, useState } from 'react';
+import { createContext, useMemo, useState } from 'react';
 
 export const UpdaterContext = createContext({});
 
 export const UpdaterContextProvider = ({ children }) => {
     const [shouldUpdate, setShouldUpdate] = useState(false);
+    const contextValue = useMemo(
+        () => ({
+            shouldUpdate,
+            setShouldUpdate,
+        }),
+        [shouldUpdate],
+    );
     return (
-        <UpdaterContext.Provider value={{ shouldUpdate, setShouldUpdate }}>
+        <UpdaterContext.Provider value={contextValue}>
             {children}
         </UpdaterContext.Provider>
     );
