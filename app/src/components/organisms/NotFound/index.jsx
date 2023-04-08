ccb13c89789d6ca94de59d4d129abcc33caf7706
sudjoao <isudjoao@gmail.com>
2023-04-08 18:49:53 -0300

    chore: update notfound style

--- app/src/components/organisms/NotFound/index.jsx
+++ app/src/components/organisms/NotFound/index.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import NotFoundImage from '../../../../assets/images/notFoundImage';
-import { styles } from './styles';
 import { Text, View } from 'react-native';
 
-export const NotFound = ({ title = 'Sem resultados', body }) => {
+export const NotFound = ({
+    title = 'Sem resultados',
+    body,
+    size = 'regular',
+}) => {
+    const sizeOptions = {
+        image: {
+            width: {
+                small: '100',
+                regular: '180',
+                large: '300',
+            },
+            height: {
+                small: '100',
+                regular: '180',
+                large: '300',
+            },
+        },
+        title: {
+            small: 'text-base',
+            regular: 'text-lg ',
+            large: 'text-xl',
+        },
+        body: {
+            small: 'text-sm',
+            regular: 'text-base ',
+            large: 'text-lg',
+        },
+    };
+
     return (
-        <View style={styles.notFoundContainer}>
-            <NotFoundImage />
-            <Text style={styles.notFoundTitle}>{title}</Text>
-            <Text style={styles.notFoundText}>{body}</Text>
+        <View className="flex-1 items-center">
+            <NotFoundImage
+                width={sizeOptions.image.width[size]}
+                height={sizeOptions.image.height[size]}
+            />
+            <Text
+                className={`${sizeOptions.title[size]} font-semibold text-center`}
+            >
+                {title}
+            </Text>
+            <Text className={`${sizeOptions.body[size]} text- text-center`}>
+                {body}
+            </Text>
         </View>
     );
 };
