306df36ff459a9e0c53fb392094882ea3edacb5d
sudjoao <isudjoao@gmail.com>
2023-04-09 07:46:24 -0300

    chore: filter front to show just not entity users

--- app/src/components/profileList/index.jsx
+++ app/src/components/profileList/index.jsx
@@ -4,20 +4,21 @@ import styles from './styles';
 import { UserListItem } from '../molecules/UserListItem';
 import { Divider } from '../atoms/Divider';
 export default function ProfileList({ usersProfile, navigation }) {
+    const filteredUsers = usersProfile?.filter((user) => user.cpf);
     const handlenavigate = (profile) =>
         navigation.navigate('socialUserProfile', {
             userId: profile.userId,
         });
     return (
         <View style={styles.userList}>
-            {usersProfile &&
-                usersProfile.map((profile, i) => (
+            {filteredUsers &&
+                filteredUsers.map((profile, i) => (
                     <Pressable
                         key={profile._id}
                         onPress={() => handlenavigate(profile)}
                     >
                         <UserListItem user={profile} />
-                        {i != usersProfile.length - 1 && <Divider />}
+                        {i != filteredUsers.length - 1 && <Divider />}
                     </Pressable>
                 ))}
         </View>
