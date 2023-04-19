bc720fbbf2545076462f2c3a23476328bf791b87
sudjoao <isudjoao@gmail.com>
2023-04-19 16:20:02 -0300

    chore: add update when navigate to other user profile

--- app/src/components/profileList/index.jsx
+++ app/src/components/profileList/index.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Pressable } from 'react-native';
 import styles from './styles';
 import { UserListItem } from '../molecules/UserListItem';
 import { Divider } from '../atoms/Divider';
 import { useNavigation } from '@react-navigation/native';
+import { UpdaterContext } from '../../store/contexts/updaterContext';
 
 export default function ProfileList({ usersProfile, filterList = false }) {
     const navigation = useNavigation();
+    const { setShouldUpdate } = useContext(UpdaterContext);
 
     const filteredUsers = filterList
         ? usersProfile?.filter((user) => user.cpf)
         : usersProfile;
 
-    const handlenavigate = (profile) =>
+    const handlenavigate = (profile) => {
+        setShouldUpdate(true);
         navigation.navigate('socialUserProfile', {
             userId: profile.userId,
         });
+    };
 
     return (
         <View style={styles.userList}>
