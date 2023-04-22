ce70040ceb3fb7c072efaeb94675902123735791
sudjoao <isudjoao@gmail.com>
2023-04-22 12:00:02 -0300

    feat: create badge service and it context

---
+++ app/src/services/Badge.js
@@ -0,0 +1,23 @@
+import api from '../services/Api';
+
+class BadgeService {
+    constructor() {}
+
+    async getUserBadges(userId) {
+        const response = await api.get(`/badges?userId=${userId}`);
+        return response.data;
+    }
+    async getBadgesHistory(userId) {
+        const response = await api.get(`/badges/history?userId=${userId}`);
+        return response.data;
+    }
+    async increaseUserBadge(userId, category) {
+        const response = await api.post('/badges/', { userId, category });
+        return response.data;
+    }
+}
+
+const badgeService = new BadgeService();
+Object.freeze(badgeService);
+
+export default badgeService;
