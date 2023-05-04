1be9bd9091c54af58b14d7b162936f148c5f4000
sudjoao <isudjoao@gmail.com>
2023-05-03 22:06:55 -0300

    feat: create base and badge modal

---
+++ app/src/components/modals/BaseModal/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal, View } from 'react-native';
+import { FloatingIconButton } from '../../molecules/FloatingIconButton';
+
+export const BaseModal = ({
+    isVisible,
+    animationType = 'fade',
+    children,
+    onCloseModal,
+}) => {
+    return (
+        <Modal
+            visible={isVisible}
+            onRequestClose={onCloseModal}
+            animationType={animationType}
+            transparent
+        >
+            <View className="flex-1 w-screen h-screen backdrop-blur-sm bg-black/30 items-center justify-center px-6">
+                <View className="bg-white rounded-lg px-6 pt-8 pb-6 w-full">
+                    <FloatingIconButton
+                        iconName={'close'}
+                        onPress={onCloseModal}
+                    />
+                    {children}
+                </View>
+            </View>
+        </Modal>
+    );
+};
