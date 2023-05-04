f471e8dbac853ecf80980c20d5cf5e5086db2d43
sudjoao <isudjoao@gmail.com>
2023-05-03 22:06:32 -0300

    feat: create circle badge component

--- app/src/components/molecules/BadgeCard/index.jsx
+++ app/src/components/molecules/BadgeCard/index.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { CircleBadge } from '../../atoms/CircleBadge';
 
 export const BadgeCard = ({
     badgeTemplate,
     showLevel = false,
     hidden = false,
 }) => {
-    const iconBackgrounds = {
-        1: 'bg-first-rank',
-        2: 'bg-second-rank',
-        3: 'bg-thirt-rank',
-    };
-
     const getTitle = () => {
         return `${badgeTemplate.name.split(' ')[0]} ${badgeTemplate.rank}`;
     };
 
     const title = showLevel ? `Nível ${badgeTemplate.rank}` : getTitle();
-    const selectedIconBackground = hidden
-        ? 'bg-gray'
-        : iconBackgrounds[badgeTemplate.rank];
 
     const hiddenBadgeInfo = {
         iconName: 'help',
@@ -32,14 +24,11 @@ export const BadgeCard = ({
 
     return (
         <View className="rounded-md p-4 bg-white shadow-md shadow-black items-center w-36 mx-2 h-48 justify-around">
-            <View className={`rounded-full p-2 ${selectedIconBackground}`}>
-                <Icon
-                    name={badgeInfo.iconName}
-                    color={'white'}
-                    size={34}
-                    type={hidden ? 'material-community' : 'material'}
-                />
-            </View>
+            <CircleBadge
+                badgeIcon={badgeInfo.iconName}
+                rank={badgeTemplate.rank}
+                isHidden={hidden}
+            />
             <View>
                 <Text
                     className="font-ms-bold text-sm text-black text-center"
