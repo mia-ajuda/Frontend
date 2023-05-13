af3295eb59d1e3d16dcdd2ee4d6032c4afbf8594
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-05-12 22:57:47 -0300

    feat: add custom styles to default button atom

--- app/src/components/atoms/DefaultButton/index.jsx
+++ app/src/components/atoms/DefaultButton/index.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Pressable, Text } from 'react-native';
-import colors from '../../../../assets/styles/colorVariables';
 
 export const DefaultButton = ({
     title,
     onPress,
     disabled,
     customStyle = '',
+    textStyle = '',
 }) => {
     return (
         <Pressable
             onPress={onPress}
             className={`w-full py-3 rounded-md bg-primary ${customStyle}`}
             disabled={disabled}
-            android_ripple={{ color: colors.primaryContrast }}
+            android_ripple={{ color: '#D2D2D2' }}
         >
-            <Text className="text-center text-light font-ms-semibold text-lg">
+            <Text
+                className={`text-center text-light font-ms-semibold text-lg ${textStyle}`}
+            >
                 {title}
             </Text>
         </Pressable>
