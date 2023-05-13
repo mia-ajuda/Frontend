9eeac062e5752d5ee6543c88e46791a561a335df
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-05-12 23:29:44 -0300

    refactor: refact my request help screen

---
+++ app/src/pages/ActivitiesPages/MyRequestedHelp/MyRequestHelpDescription/index.jsx
@@ -0,0 +1,154 @@
+import React, { Fragment, useContext, useEffect, useState } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import { DefaultButtonWithBadges } from '../../../../components/molecules/DefaultButtonWithBagdes';
+import { HelpScreenLayout } from '../../../../components/templates/HelpScreenLayout';
+import { UserContext } from '../../../../store/contexts/userContext';
+import callService from '../../../../services/callService';
+import helpService from '../../../../services/Help';
+import { LoadingContext } from '../../../../store/contexts/loadingContext';
+import { ExpansiveModal } from '../../../../components/modals/expansiveModal';
+import { UserCard } from '../../../../components/atoms/UserCard';
+import { DefaultButton } from '../../../../components/atoms/DefaultButton';
+import openWhatsapp from '../../../../utils/openWhatsapp';
+import callNumber from '../../../../utils/callNumber';
+import ConfirmationModal from '../../../../components/modals/confirmationModal';
+import { HelpContext } from '../../../../store/contexts/helpContext';
+
+export default function HelpDescription({
+    route,
+    navigation,
+    setConfirmationModalVisible,
+    confirmationModalVisible,
+    setHelp,
+    help,
+}) {
+    const { helpId, routeId } = route.params;
+    const { user } = useContext(UserContext);
+    const { isLoading, setIsLoading } = useContext(LoadingContext);
+    const [updateData, setUpdateData] = useState(true);
+    const [showPossibleHelpers, setShowPossibleHelpers] = useState(false);
+    const { finishHelpByOwner } = useContext(HelpContext);
+
+    useEffect(() => {
+        async function setupPage() {
+            setIsLoading(true);
+            const helpTemp = await callService(
+                helpService,
+                `get${routeId}WithAggregationById`,
+                [helpId],
+            );
+
+            setHelp(helpTemp);
+            setIsLoading(false);
+        }
+        if (updateData) {
+            setupPage();
+            setUpdateData(false);
+        }
+    }, [updateData]);
+
+    const renderButtons = () => {
+        const possibleHelpersBadgeValue =
+            (help.possibleHelpers?.length || 0) +
+            (help.possibleEntities?.length || 0);
+
+        return (
+            <View className="mt-6">
+                {help.helperId ? (
+                    renderMyHelper()
+                ) : (
+                    <DefaultButtonWithBadges
+                        title="Possíveis Ajudantes"
+                        onPress={() => setShowPossibleHelpers(true)}
+                        badgeValue={possibleHelpersBadgeValue}
+                        disabled={possibleHelpersBadgeValue <= 0}
+                    />
+                )}
+            </View>
+        );
+    };
+
+    const renderHelperCard = () => {
+        return (
+            <Fragment>
+                <Text className="text-lg font-ms-semibold mb-2">Ajudante</Text>
+                <UserCard userId={help.helperId} />
+            </Fragment>
+        );
+    };
+
+    const renderHelperContactButtons = () => {
+        return (
+            <View className="mt-4">
+                <DefaultButton
+                    title="Inicie uma conversa"
+                    onPress={() => openWhatsapp(user.phone)}
+                />
+                <DefaultButton
+                    title="Faça uma ligação"
+                    customStyle="bg-white mt-2"
+                    textStyle="text-black"
+                    onPress={() => callNumber(user.phone)}
+                />
+            </View>
+        );
+    };
+
+    const renderMyHelper = () => {
+        if (help.status != 'finished')
+            return (
+                <Fragment>
+                    {renderHelperCard()}
+                    {renderHelperContactButtons()}
+                </Fragment>
+            );
+    };
+
+    const ownerPhoto = (help && help.user && help.user.photo) || user.photo;
+
+    const getAllPossibleHelpers = () => {
+        const { possibleHelpers, possibleEntities } = help;
+        return [...possibleHelpers, ...possibleEntities];
+    };
+
+    const finishHelpByOwnerAction = () => {
+        finishHelpByOwner(helpId);
+        navigation.reset({
+            index: 0,
+            routes: [{ name: 'activitiesDrawer' }],
+        });
+    };
+
+    return (
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <ConfirmationModal
+                visible={confirmationModalVisible && !isLoading}
+                setVisible={setConfirmationModalVisible}
+                action={finishHelpByOwnerAction}
+                message={
+                    'Você tem certeza que deseja finalizar este pedido de ajuda?'
+                }
+            />
+            {help && (
+                <HelpScreenLayout
+                    help={help}
+                    ownerPhoto={ownerPhoto}
+                    navigation={navigation}
+                >
+                    {renderButtons()}
+                </HelpScreenLayout>
+            )}
+            {showPossibleHelpers && (
+                <ExpansiveModal
+                    setShowModal={setShowPossibleHelpers}
+                    userList={getAllPossibleHelpers()}
+                    title="Possíveis ajudantes"
+                    method="chooseHelper"
+                    helpId={helpId}
+                    showButton={true}
+                    setUpdateData={setUpdateData}
+                />
+            )}
+        </ScrollView>
+    );
+}
