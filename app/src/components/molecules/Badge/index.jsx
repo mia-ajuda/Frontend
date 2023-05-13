e66d5f3135975b29bd2b95132899e5995118b97d
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-05-12 23:03:58 -0300

    feat: create category list based on badges

--- app/src/components/molecules/Badge/index.jsx
+++ app/src/components/molecules/Badge/index.jsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function Badge({ title }) {
+export default function Badge({ title, size = 'small' }) {
+    const viewSize = {
+        small: 'px-2 py-1 m-0.5',
+        medium: 'px-3 py-1.5 m-0.5',
+        large: 'px-3.5 py-2 m-1',
+    };
+
+    const textSize = {
+        small: 'text-xss',
+        medium: 'text-xs',
+        large: 'text-sm',
+    };
+
     return (
-        <View className="bg-secondary px-2 py-1 rounded-md">
-            <Text className="text-xs font-ms-semibold text-black">{title}</Text>
+        <View className={`bg-secondary rounded-lg ${viewSize[size]}`}>
+            <Text className={`font-ms-semibold ${textSize[size]}`}>
+                {title}
+            </Text>
         </View>
     );
 }
