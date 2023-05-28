b65d00a4bc9117057c8e3f392a700aaddf6bf19f
sudjoao <isudjoao@gmail.com>
2023-05-28 17:36:25 -0300

    feat: improve open whatsapp logic

--- app/src/utils/openWhatsapp.js
+++ app/src/utils/openWhatsapp.js
@@ -1,8 +1,11 @@
 import { Linking } from 'react-native';
+import { alertMessage } from './Alert';
 
 export default function openWhatsapp(
     phoneNumber,
     message = 'Olá, precisa de ajuda?',
 ) {
-    Linking.openURL(`whatsapp://send?phone=${phoneNumber}&text=${message}`);
+    Linking.openURL(
+        `whatsapp://send?phone=${phoneNumber}&text=${message}`,
+    ).catch(() => alertMessage('Não foi possível abrir o Whatsapp.'));
 }
