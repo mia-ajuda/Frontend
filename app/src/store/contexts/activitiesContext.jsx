c6a73467081a03315f5ca33717126fb9f6f83dc4
sudjoao <isudjoao@gmail.com>
2023-05-27 21:10:16 -0300

    feat: create logic to interact with activity

--- app/src/store/contexts/activitiesContext.jsx
+++ app/src/store/contexts/activitiesContext.jsx
@@ -1,24 +1,19 @@
-import React, { createContext, useMemo, useState } from 'react';
+import React, { createContext, useContext, useMemo, useState } from 'react';
 import callService from '../../services/callService';
 import helpService from '../../services/Help';
 import campaignService from '../../services/Campaign';
-import { ActivityBottomSheet } from '../../components/modals/ActivityBottomSheet';
+import { LoadingContext } from './loadingContext';
+import { HelpOfferContext } from './helpOfferContext';
+import { HelpContext } from './helpContext';
+import { UserContext } from './userContext';
 
 export const ActivitiesContext = createContext({});
 
 export const ActivitiesContextProvider = ({ children }) => {
-    const [showActivityModal, setShowActivityModal] = useState(false);
-    const [activityInfo, setActivityInfo] = useState();
-
-    const handleShowModal = (id, ownerId, type) => {
-        setActivityInfo({ id, ownerId, type });
-        setShowActivityModal(true);
-    }
-
-    const handleHideModal = () => {
-        setShowActivityModal(false);
-        setActivityInfo();
-    }
+    const { setIsLoading } = useContext(LoadingContext)
+    const { setHelpOfferList } = useContext(HelpOfferContext);
+    const { helpList, dispatch } = useContext(HelpContext);
+    const { user } = useContext(UserContext);
 
     const activitiesServices = {
         help: helpService,
@@ -32,6 +27,10 @@ export const ActivitiesContextProvider = ({ children }) => {
             offer: 'getHelpOfferWithAggregationById',
             campaign: 'getCampaignById',
         },
+        interact: {
+            help: 'offerHelp',
+            offer: 'participateHelpOffer',
+        }
     };
 
     async function getActitivtieById(activityType, activityId) {
@@ -43,21 +42,41 @@ export const ActivitiesContextProvider = ({ children }) => {
         return activityInfo;
     }
 
+    function removeElementFromMap(activityType, activityId) {
+        if (activityType == 'offer') {
+            setHelpOfferList((currentValue) =>
+                currentValue.filter((helpOffer) => helpOffer._id != activityId),
+            );
+        } else {
+            const filteredHelpList = helpList.filter((mapHelp) => mapHelp._id != activityId);
+            dispatch({ type: actions.help.storeList, helps: filteredHelpList });
+        }
+    }
+
+    async function interactWithActivity(activityType, activityId, finishLoading = false) {
+        setIsLoading(true);
+        const response = await callService(
+            activitiesServices[activityType],
+            servicesEndpoints.interact[activityType],
+            [activityId, user._id],
+        );
+        if (!response.error)
+            removeElementFromMap(activityType, activityId)
+        if (finishLoading)
+            setIsLoading(false);
+        return response;
+    }
+
     const contextValue = useMemo(() => {
         return {
             getActitivtieById,
-            handleHideModal,
-            handleShowModal,
-            setShowActivityModal
+            interactWithActivity
         };
-    }, [getActitivtieById, handleHideModal, handleShowModal, setShowActivityModal]);
+    }, [getActitivtieById, interactWithActivity]);
 
     return (
         <ActivitiesContext.Provider value={contextValue}>
             {children}
-            {showActivityModal && (
-                <ActivityBottomSheet selectedActivity={activityInfo} setShowModal={setShowActivityModal} />
-            )}
         </ActivitiesContext.Provider>
     );
 };
