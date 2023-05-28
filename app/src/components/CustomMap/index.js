7ada7d35dafa2c5475a2834b6938ad3cba430db5
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-05-28 11:02:38 -0300

    refactor: add marker animation to custom map

--- app/src/components/CustomMap/index.js
+++ app/src/components/CustomMap/index.js
@@ -1,27 +1,33 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import MapView from 'react-native-maps';
 import mapstyle from '../../../assets/styles/mapstyle';
 
 export default function CustomMap({
     children,
     initialRegion,
-    region,
+    animateToRegion,
     ...rest
 }) {
     const mapRef = useRef(null);
+
+    useEffect(() => {
+        if (animateToRegion) {
+            mapRef.current.animateToRegion(animateToRegion, 1000);
+        }
+    }, [animateToRegion]);
 
     return (
         <MapView
             ref={mapRef}
             provider="google"
             initialRegion={initialRegion}
-            region={region}
             className="w-full h-full"
             showsUserLocation={true}
             onPress={() =>
                 rest.setHelpListVisible && rest.setHelpListVisible(false)
             }
             customMapStyle={mapstyle.day.map}
+            showsMyLocationButton={false} // change
         >
             {children}
         </MapView>
