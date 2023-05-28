4cd9d6899682e27dbcf3d58dfa65dc4e0885985d
sudjoao <isudjoao@gmail.com>
2023-05-27 21:08:33 -0300

    feat: create context to show bottom sheet activity modal

---
+++ app/src/store/contexts/activityBottomSheetContext.jsx
@@ -0,0 +1,36 @@
+import React, { createContext, useMemo, useState } from 'react';
+import { ActivityBottomSheet } from '../../components/modals/ActivityBottomSheet';
+
+export const ActivityBottomSheetContext = createContext({});
+
+export const ActivityBottomSheetContextProvider = ({ children }) => {
+    const [showActivityModal, setShowActivityModal] = useState(false);
+    const [activityInfo, setActivityInfo] = useState();
+
+    const handleShowModal = (id, ownerId, type) => {
+        setActivityInfo({ id, ownerId, type });
+        setShowActivityModal(true);
+    }
+
+    const handleHideModal = () => {
+        setShowActivityModal(false);
+        setActivityInfo();
+    }
+
+    const contextValue = useMemo(() => {
+        return {
+            handleHideModal,
+            handleShowModal,
+            setShowActivityModal
+        };
+    }, [handleHideModal, handleShowModal, setShowActivityModal]);
+
+    return (
+        <ActivityBottomSheetContext.Provider value={contextValue}>
+            {children}
+            {showActivityModal && (
+                <ActivityBottomSheet selectedActivity={activityInfo} setShowModal={setShowActivityModal} />
+            )}
+        </ActivityBottomSheetContext.Provider>
+    );
+};
