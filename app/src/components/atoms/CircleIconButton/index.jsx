71eff297d5b91064fe1a28c88c97b6439ee3676d
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-06-01 19:11:21 -0300

    feat: add more versatility to circle icon button

--- app/src/components/atoms/CircleIconButton/index.jsx
+++ app/src/components/atoms/CircleIconButton/index.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import { TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 
-export const CircleIconButton = ({ icon, onPress }) => {
+export const CircleIconButton = ({
+    icon,
+    onPress,
+    iconSize = 'base',
+    color,
+}) => {
+    const iconSizeVariant = {
+        sm: 16,
+        base: 20,
+        lg: 24,
+        xl: 28,
+        '2xl': 32,
+    };
+
+    const buttonColor = color ? color : '';
+
     return (
         <TouchableOpacity onPress={onPress}>
-            <View className="p-1 rounded-full border-[#BBBBBB] border-[0.5px] ">
-                <Icon name={icon} size={20} />
+            <View
+                className={`p-1 rounded-full border-[#BBBBBB] border-[0.5px] ${buttonColor}`}
+            >
+                <Icon name={icon} size={iconSizeVariant[iconSize]} />
             </View>
         </TouchableOpacity>
     );
