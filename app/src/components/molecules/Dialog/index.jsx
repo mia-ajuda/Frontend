95381dfbbfd541fa948af1ac3de96dc938e796d1
sudjoao <isudjoao@gmail.com>
2023-06-03 21:46:43 -0300

    feat: create dialog molecule

---
+++ app/src/components/molecules/Dialog/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Modal, Pressable, Text, View } from 'react-native';
+
+export const Dialog = ({
+    isVisible,
+    onCloseDialog,
+    title,
+    description,
+    cancelText,
+    animationType = 'slide',
+    confirmText = 'Ok',
+    onCofirmPress,
+}) => {
+    return (
+        <Modal
+            visible={isVisible}
+            onRequestClose={onCloseDialog}
+            animationType={animationType}
+            transparent
+        >
+            <View className="flex-1 w-screen h-screen backdrop-blur-sm bg-black/30 items-center justify-center px-6">
+                <View className="bg-light rounded-xl p-6">
+                    <Text className="font-ms-semibold text-black text-left pb-4">
+                        {title}
+                    </Text>
+                    <Text className="font-ms-regular text-black text-left pb-6">
+                        {description}
+                    </Text>
+                    <View className="flex flex-row justify-end">
+                        {cancelText && (
+                            <Pressable onPress={onCloseDialog}>
+                                <Text className="text-primary font-ms-semibold mr-10">
+                                    {cancelText}
+                                </Text>
+                            </Pressable>
+                        )}
+                        <Pressable onPress={onCofirmPress}>
+                            <Text className="text-primary font-ms-semibold">
+                                {confirmText}
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
