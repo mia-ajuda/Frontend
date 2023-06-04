280e5bc4855db2cfe76f726584d9770d34d77b52
sudjoao <isudjoao@gmail.com>
2023-06-04 17:00:00 -0300

    feat: add loading logic and not found component

--- app/src/pages/Feedback/index.jsx
+++ app/src/pages/Feedback/index.jsx
@@ -1,17 +1,21 @@
 import React, { useCallback, useContext, useState } from 'react';
-import { Image, Text, View } from 'react-native';
+import { View } from 'react-native';
 import Timeline from 'react-native-timeline-flatlist';
 import colors from '../../../colors';
-import { Divider } from '../../components/atoms/Divider';
 import { FeedbackContext } from '../../store/contexts/feedbackContext';
 import { UserContext } from '../../store/contexts/userContext';
 import { useFocusEffect } from '@react-navigation/native';
 import formatDate from '../../utils/formatDate';
+import { NotFound } from '../../components/organisms/NotFound';
+import { FeedbackItem } from './FeedbackItem';
+import { LoadingContext } from '../../store/contexts/loadingContext';
 
 export const FeedbackScreen = () => {
     const { getFeedbackByReceiverId } = useContext(FeedbackContext);
     const { user } = useContext(UserContext);
+    const { isLoading } = useContext(LoadingContext);
     const [feedbacks, setFeedbacks] = useState([]);
+    const hasFeedbacks = feedbacks.length > 0;
 
     const getData = async () => {
         const data = await getFeedbackByReceiverId(user._id);
@@ -31,44 +35,35 @@ export const FeedbackScreen = () => {
         icon: feedback.sender.photo,
     }));
 
-    const renderContent = (data) => (
-        <View className="-mt-2 mb-2 w-full">
-            <View className="flex-row mb-2">
-                <Image
-                    source={{
-                        uri: `data:image/png;base64,${data.icon}`,
-                    }}
-                    className="rounded-full h-10 w-10"
-                />
-                <View className="flex-1 ml-2">
-                    <Text className="font-ms-bold text-black mb-[0.5px]">
-                        {data.title}
-                    </Text>
-                    <Text className="font-ms-regular text-black text-xs">
-                        {data.description}
-                    </Text>
-                </View>
-            </View>
-            <Divider marginHorizontal={4} />
-        </View>
+    const renderContent = (data, sectionID) => (
+        <FeedbackItem data={data} key={sectionID} />
     );
 
     return (
         <View className="flex-1  h-full p-4">
-            <Timeline
-                data={mappedFeedbacks}
-                lineWidth={4}
-                lineColor={colors.primary[300]}
-                circleColor={colors.primary.DEFAULT}
-                timeStyle={{
-                    backgroundColor: colors.primary.DEFAULT,
-                    padding: 4,
-                    borderRadius: 12,
-                    color: colors.light,
-                    fontFamily: 'montserrat-semibold',
-                }}
-                renderDetail={renderContent}
-            />
+            {hasFeedbacks && (
+                <Timeline
+                    data={mappedFeedbacks}
+                    lineWidth={4}
+                    lineColor={colors.primary[300]}
+                    circleColor={colors.primary.DEFAULT}
+                    timeStyle={{
+                        backgroundColor: colors.primary.DEFAULT,
+                        padding: 4,
+                        borderRadius: 12,
+                        color: colors.light,
+                        fontFamily: 'montserrat-semibold',
+                    }}
+                    renderDetail={renderContent}
+                />
+            )}
+            {!hasFeedbacks && !isLoading && (
+                <NotFound
+                    title="Nenhum feedback recebido"
+                    body="Você ainda não recebeu nenhum feedback"
+                    size="large"
+                />
+            )}
         </View>
     );
 };
