2f8d4e01d202fdf8fb57d57d5f39ec76709671c0
sudjoao <isudjoao@gmail.com>
2023-06-10 10:25:18 -0300

    feat: use expo camera to take pictures in the app

---
+++ app/src/components/organisms/DefaultCamera/index.jsx
@@ -0,0 +1,59 @@
+import { Camera, CameraType } from 'expo-camera';
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import { DefaultButton } from '../../atoms/DefaultButton';
+
+export const DefaultCamera = ({
+    cameraRef,
+    handleTakePicture,
+    photo,
+    setPhoto,
+    handleSubmitPhoto,
+    preview = false
+}) => {
+    const renderCamera = () => (
+        <>
+            <View className="flex-1 rounded-full w-80 max-h-80 overflow-hidden">
+                <Camera
+                    ratio="4:3"
+                    type={CameraType.front}
+                    className="flex-1 w-80 max-h-80"
+                    ref={cameraRef}
+                />
+            </View>
+            <DefaultButton title="Tirar foto" onPress={handleTakePicture} />
+        </>
+    );
+
+    const renderPhotoPreview = () => (
+        <>
+            <Text className="font-ms-semibold text-black mb-4 text-xl">
+                Essa foto ficou boa?
+            </Text>
+            <Image
+                source={{
+                    uri: `data:image/png;base64,${photo.base64}`,
+                }}
+                className="w-80 h-80 mb-12 rounded-full"
+            />
+            <View className="flex-1 w-full px-4">
+                <DefaultButton
+                    title="Salvar"
+                    onPress={() => handleSubmitPhoto(photo)}
+                    margin="mb-4"
+                />
+                <DefaultButton
+                    variant="secondary"
+                    title="Tirar outra"
+                    onPress={() => setPhoto(null)}
+                />
+            </View>
+        </>
+    );
+
+    return (
+        <View className="flex-1 items-center">
+            {photo && preview ? renderPhotoPreview() : renderCamera()}
+        </View>
+    );
+};
--- app/src/pages/AuthPages/Photo/index.js
+++ app/src/pages/AuthPages/Photo/index.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Icon } from 'react-native-elements';
 import styles from './styles';
 import Container from '../../../components/Container';
+import { Camera } from 'expo-camera';
 
 import { alertMessage, alertSuccess } from '../../../utils/Alert';
 import { UserContext } from '../../../store/contexts/userContext';
@@ -13,20 +14,16 @@ import callService from '../../../services/callService';
 import actions from '../../../store/actions';
 import { ActivityBottomSheetContext } from '../../../store/contexts/activityBottomSheetContext';
 import { useNavigationState } from '@react-navigation/native';
+import { DefaultCamera } from '../../../components/organisms/DefaultCamera';
 export default function Photo({ route, navigation }) {
     const state = useNavigationState((state) => state);
     const { userDataFromAddressPage } = route.params;
     const { dispatch, isEntity } = useContext(UserContext);
     const { handleShowModal } = useContext(ActivityBottomSheetContext);
-
-    async function requestPermission() {
-        const permissionResult =
-            await ImagePicker.requestCameraPermissionsAsync();
-        if (permissionResult.granted === false) {
-            alertMessage('É preciso permissão para colocar uma foto.');
-            return;
-        }
-    }
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    const [showCamera, setShowCamera] = useState(false);
+    const [photo, setPhoto] = useState();
+    const cameraRef = useRef();
     const { routes } = state;
 
     const handleSaveInfo = async (pickerResult) => {
@@ -65,23 +62,20 @@ export default function Photo({ route, navigation }) {
         }
     };
 
-    async function openImagePickerAsync() {
-        requestPermission();
+    const openImagePickerAsync = async () => {
+        await requestPermission();
+        if (permission.granted) {
+            setShowCamera(true);
+        }
+    };
 
-        const pickerResult = await ImagePicker.launchCameraAsync({
-            base64: true,
-            allowsEditing: true,
-            quality: 0.5,
-        });
-        if (pickerResult.canceled === true) {
+    const pickImageFromGallery = async () => {
+        const permissionResult =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (!permissionResult.granted) {
+            alertMessage('É preciso permissão para colocar uma foto.');
             return;
         }
-        handleSaveInfo(pickerResult);
-    }
-
-    async function pickImageFromGallery() {
-        requestPermission();
-
         let pickerResult = await ImagePicker.launchImageLibraryAsync({
             allowsEditing: true,
             quality: 0.5,
@@ -93,53 +87,70 @@ export default function Photo({ route, navigation }) {
             return;
         }
         handleSaveInfo(pickerResult);
-    }
-    const renderCameraButton = () => (
-        <TouchableOpacity
-            onPress={openImagePickerAsync}
-            style={styles.pickPhotoButton}
-        >
-            <View style={styles.button}>
-                <Icon name={'camera-alt'} color="gray" />
-            </View>
-            <Text style={styles.pickerText}>Abrir camera</Text>
-        </TouchableOpacity>
-    );
-    const renderGalleryButton = () => {
-        if (isEntity) {
-            return (
-                <TouchableOpacity
-                    onPress={pickImageFromGallery}
-                    style={styles.pickPhotoButton}
-                >
-                    <View style={styles.button}>
-                        <Icon name={'photo-library'} color="gray" />
-                    </View>
-
-                    <Text style={styles.pickerText}>Abrir galeria</Text>
-                </TouchableOpacity>
-            );
-        }
     };
+
+    const handleTakePicture = async () => {
+        const response = await cameraRef.current.takePictureAsync({
+            base64: true,
+            width: 320,
+            height: 320,
+        });
+        setPhoto(response);
+    };
+
+    const renderCamera = () => (
+        <DefaultCamera
+            cameraRef={cameraRef}
+            handleTakePicture={handleTakePicture}
+            handleSubmitPhoto={handleSaveInfo}
+            photo={photo}
+            setPhoto={setPhoto}
+            preview
+        />
+    );
+
+    const renderInstructions = () => (
+        <ImageBackground
+            source={require('../../../images/catPhoto.png')}
+            style={styles.logo}
+        >
+            <Container>
+                <View style={styles.textView}>
+                    <Text style={styles.text}>
+                        Também precisamos de uma foto sua, é só clicar na camêra
+                        aqui em baixo!
+                    </Text>
+                </View>
+                <View style={styles.btnView}>
+                    <TouchableOpacity
+                        onPress={openImagePickerAsync}
+                        style={styles.pickPhotoButton}
+                    >
+                        <View style={styles.button}>
+                            <Icon name={'camera-alt'} color="gray" />
+                        </View>
+                        <Text style={styles.pickerText}>Abrir camera</Text>
+                    </TouchableOpacity>
+                    {isEntity && (
+                        <TouchableOpacity
+                            onPress={pickImageFromGallery}
+                            style={styles.pickPhotoButton}
+                        >
+                            <View style={styles.button}>
+                                <Icon name={'photo-library'} color="gray" />
+                            </View>
+
+                            <Text style={styles.pickerText}>Abrir galeria</Text>
+                        </TouchableOpacity>
+                    )}
+                </View>
+            </Container>
+        </ImageBackground>
+    );
+
     return (
         <View style={styles.container}>
-            <ImageBackground
-                source={require('../../../images/catPhoto.png')}
-                style={styles.logo}
-            >
-                <Container>
-                    <View style={styles.textView}>
-                        <Text style={styles.text}>
-                            Também precisamos de uma foto sua, é só clicar na
-                            camêra aqui em baixo!
-                        </Text>
-                    </View>
-                    <View style={styles.btnView}>
-                        {renderCameraButton()}
-                        {renderGalleryButton()}
-                    </View>
-                </Container>
-            </ImageBackground>
+            {showCamera ? renderCamera() : renderInstructions()}
         </View>
     );
 }
