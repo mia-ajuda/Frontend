d64d9218422237c98498bb3414552e0de68771d3
sudjoao <isudjoao@gmail.com>
2023-06-23 06:28:31 -0300

    feat: create default timeline organism

--- app/src/components/molecules/TimelineItem/index.jsx
+++ app/src/components/molecules/TimelineItem/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Image, Text, View } from 'react-native';
-import { Divider } from '../../../components/atoms/Divider';
+import { Divider } from '../../atoms/Divider';
 
-export const FeedbackItem = ({ data }) => {
+export const TimelineItem = ({ data, hasImage }) => {
     return (
         <View className="-mt-2 mb-2 w-full">
             <View className="flex-row mb-2">
-                <Image
+                {hasImage && <Image
                     source={{
                         uri: `data:image/png;base64,${data.icon}`,
                     }}
                     className="rounded-full h-10 w-10"
-                />
+                />}
                 <View className="flex-1 ml-2">
                     <Text className="font-ms-bold text-black mb-[0.5px]">
                         {data.title}
---
+++ app/src/components/organisms/DefaultTimeline/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Timeline from 'react-native-timeline-flatlist'
+import colors from '../../../../colors'
+import { TimelineItem } from '../../molecules/TimelineItem'
+
+export const DefaultTimeline = ({ data, lineWidth = 4, hasImage = false, useIcon = false }) => {
+
+    const renderContent = (data, sectionID) => (
+        <TimelineItem data={data} key={sectionID} hasImage={hasImage} />
+    );
+
+    return (
+        <Timeline
+            data={data}
+            lineWidth={lineWidth}
+            lineColor={colors.primary[300]}
+            circleColor={useIcon? 'transparent' : colors.primary.DEFAULT}
+            circleSize={20}
+            innerCircle={useIcon? 'icon' : 'none'}
+            timeStyle={{
+                backgroundColor: colors.primary.DEFAULT,
+                padding: 4,
+                borderRadius: 12,
+                color: colors.light,
+                fontFamily: 'montserrat-semibold',
+                fontSize: 12,
+                textAlign: `center`,
+            }}
+            renderDetail={renderContent}
+        />
+    )
+}
