b213b18aa45defc4428f6f169d178f2a318cda03
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-06-28 20:42:24 -0300

    refactor: change activity list request query params

--- app/src/services/Activity.js
+++ app/src/services/Activity.js
@@ -3,10 +3,13 @@ import api from '../services/Api';
 class ActivityService {
     async getActivityList(id, coords, categoryId, activityId) {
         const { longitude, latitude } = coords;
-        const url =
-            `/activity/list?id=${id}&coords=${longitude},${latitude}` +
-            (categoryId ? `&categoryId=${categoryId}` : '') +
-            (activityId ? `&activityId=${activityId}` : '');
+        const queryParams = new URLSearchParams({
+            id,
+            coords: `${longitude},${latitude}`,
+            ...(categoryId && { categoryId }),
+            ...(activityId && { activityId }),
+        });
+        const url = `/activity/list?${queryParams.toString()}`;
 
         const activityList = await api.get(url);
         return activityList.data;
