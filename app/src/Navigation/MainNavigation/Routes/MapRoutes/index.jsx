74a1e935618226154d1ce23f7626d5c70ec10042
Guilherme Barbosa Ferreira <mrguilhermeferreira@gmail.com>
2023-07-02 02:44:53 -0300

    fix: remove unnecessary files

--- app/src/Navigation/MainNavigation/Routes/MapRoutes/index.jsx
+++ app/src/Navigation/MainNavigation/Routes/MapRoutes/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from '../../../../pages/Main';
-import MapHelpDescription from '../../../../pages/HelpPages/MapHelpDescription';
 import headerStyle from '../../MainNavigationStyles/MainStackHeaderStyle';
 import MapScreen from '../../../../pages/Main/MapScreen';
 
@@ -11,10 +10,6 @@ export const MapRoutes = () => {
     return (
         <>
             <Stack.Screen name="home" component={Main} options={headerStyle} />
-            <Stack.Screen
-                name="mapHelpDescription"
-                component={MapHelpDescription}
-            />
             <Stack.Screen
                 name="mapScreen"
                 component={MapScreen}
